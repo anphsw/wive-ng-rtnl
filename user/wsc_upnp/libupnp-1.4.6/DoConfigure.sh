5563e4b5b5e9bc632c77c089d52a95e899175c2d
sfstudio <sfstudio@mail.ru>
2010-01-31 07:59:45 +0600

    More flags for more optimize

--- user/wsc_upnp/libupnp-1.4.6/DoConfigure.sh
+++ user/wsc_upnp/libupnp-1.4.6/DoConfigure.sh
@@ -3,7 +3,7 @@
 ROOTDIR=`pwd`
 CONFOPTS="--host=mipsel-linux"
 CONFOPTS="$CONFOPTS --prefix=$ROOTDIR/filesystem"
-CFLAGS="-Os -pipe -mips32 -mtune=mips32 -Wall -Wno-unused -s -fpack-struct -Wpadded  -D__LITTLE_ENDIAN"
+CFLAGS="-Os -pipe -mips32 -mtune=mips32 -s -fpack-struct"
 CPPFLAGS="-Os"
 export $CFLAGS $CPPFLAGS
 ./configure $CONFOPTS
