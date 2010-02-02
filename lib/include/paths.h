9a3bdf24213ad3603babc66bf1bc295c6fc29104
sfstudio <sfstudio@mail.ru>
2010-02-03 02:06:58 +0600

    Job control now work

--- lib/include/paths.h
+++ lib/include/paths.h
@@ -40,7 +40,7 @@
 
 #define	_PATH_BSHELL	"/bin/sh"
 #define	_PATH_CONSOLE	"/dev/console"
-#define	_PATH_TTY	"/dev/ttyS1"
+#define	_PATH_TTY	"/dev/tty"
 #define	_PATH_CSHELL	"/bin/csh"
 #define	_PATH_DEVDB	"/var/run/dev.db"
 #define	_PATH_DEVNULL	"/dev/null"
