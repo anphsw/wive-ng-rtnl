dfee5c0b66ec1d56cec9bbdeb985a741ea843562
sfstudio <sfstudio@mail.ru>
2010-07-23 04:34:15 +0700

    loopback: calls netif_receive_skb() instead of netif_rx()

--- linux-2.6.21.x/drivers/net/loopback.c
+++ linux-2.6.21.x/drivers/net/loopback.c
@@ -158,7 +158,7 @@ static int loopback_xmit(struct sk_buff *skb, struct net_device *dev)
 	lb_stats->bytes += skb->len;
 	lb_stats->packets++;
 
-	netif_rx(skb);
+	netif_receive_skb(skb);
 
 	return 0;
 }
