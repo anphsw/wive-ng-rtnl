d217faf18d80e9098b4f584f1777df54d611d6b8
sfstudio <sfstudio@mail.ru>
2010-08-14 06:29:13 +0700

    zebra disable no need components

--- user/zebra/DoConfigure.sh
+++ user/zebra/DoConfigure.sh
@@ -2,5 +2,5 @@
 
 APROOTDIR=`pwd`
 CONFOPTS="--host=mipsel-linux"
-CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem"
+CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem --disable-ipv6 --disable-bgpd --disable-ripngd --disable-ospfd --disable-ospf6d"
 ./configure $CONFOPTS
