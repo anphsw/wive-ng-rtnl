fd7d475ec8bde17e09e98e0d343f935ffc6915c7
sfstudio <sfstudio@mail.ru>
2010-08-14 14:59:08 +0700

    Fix kernel_accept() return handling.

--- linux-2.6.21.x/net/socket.c
+++ linux-2.6.21.x/net/socket.c
@@ -2198,6 +2198,7 @@ int kernel_accept(struct socket *sock, struct socket **newsock, int flags)
 	err = sock->ops->accept(sock, *newsock, flags);
 	if (err < 0) {
 		sock_release(*newsock);
+		*newsock = NULL;
 		goto done;
 	}
 
