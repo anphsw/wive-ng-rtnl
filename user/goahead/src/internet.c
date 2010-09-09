f665058e2779fbef42ed50c7f5d807429a634770
sfstudio <sfstudio@mail.ru>
2010-09-10 04:55:56 +0700

    internet.c some codestile change

--- user/goahead/src/internet.c
+++ user/goahead/src/internet.c
@@ -773,16 +773,12 @@ void formVPNSetup(webs_t wp, char_t *path, char_t *query)
 	if (vpn_enabled[0] == '\0')
 		vpn_enabled="off";
 
-	//kill helpers firt sigterm second sigkill
-	printf("Kill helpers\n");
-	system("/bin/killall -q S70vpnhelper");
-	system("/bin/killall -q vpnhelper");
-	system("/bin/killall -q -9 S70vpnhelper");
-	system("/bin/killall -q -9 vpnhelper");
-	
+	// Now store VPN_ENABLED flag
 	// Do not set other params if VPN is turned off
-	if (strcmp(vpn_enabled, "on")==0)
+	if (strcmp(vpn_enabled, "on") && (nvram_bufset(RT2860_NVRAM, "vpnEnabled", (void *)vpn_enabled)==0))
 	{
+		printf("vpn_enabled value : %s\n", vpn_enabled);
+
 		char_t *str;
 		
 		const vpn_fetch_t *fetch = vpn_args;
@@ -816,18 +812,20 @@ void formVPNSetup(webs_t wp, char_t *path, char_t *query)
 			if (vpn_rt != NULL)
 				vpnStoreRouting(vpn_rt);
 		}
-	}
 
-	// Now store VPN_ENABLED flag
-	if (nvram_bufset(RT2860_NVRAM, "vpnEnabled", (void *)vpn_enabled)==0)
-	{
-		printf("vpn_enabled value : %s\n", vpn_enabled);
+	    nvram_commit(RT2860_NVRAM);
 	} else  {
-		printf("Set vpnEnabled error!\n");
-		return ;
+	    printf("Set vpnEnabled error!\n");
+	    return ;
 	}
-	nvram_commit(RT2860_NVRAM);
 
+
+	//kill helpers firt sigterm second sigkill
+	printf("Kill helpers\n");
+	system("/bin/killall -q S70vpnhelper");
+	system("/bin/killall -q vpnhelper");
+	system("/bin/killall -q -9 S70vpnhelper");
+	system("/bin/killall -q -9 vpnhelper");
     	sleep(3);
 	printf("Calling vpn helper...\n");
 	system("service vpnhelper restart &");
