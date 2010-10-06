4da9284a8fe4c515a914a9e8fab299e255e85bc9
sfstudio <sfstudio@mail.ru>
2010-10-06 19:07:13 +0700

    net: reorder fields of struct socket for speedup.

--- linux-2.6.21.x/include/linux/net.h
+++ linux-2.6.21.x/include/linux/net.h
@@ -108,11 +108,14 @@ enum sock_type {
 struct socket {
 	socket_state		state;
 	unsigned long		flags;
-	const struct proto_ops	*ops;
+        /*
+         * Please keep fasync_list & wait fields in the same cache line
+         */
 	struct fasync_struct	*fasync_list;
+	wait_queue_head_t	wait;
 	struct file		*file;
 	struct sock		*sk;
-	wait_queue_head_t	wait;
+	const struct proto_ops	*ops;
 	short			type;
 };
 
