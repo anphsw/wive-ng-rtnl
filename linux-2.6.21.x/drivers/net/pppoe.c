538597e67fc7ba5b8cd5b77b1019750d331741ba
sfstudio <sfstudio@mail.ru>
2010-10-11 23:44:36 +0700

    Ignore PADT packets whose destination address isn't ours

--- linux-2.6.21.x/drivers/net/pppoe.c
+++ linux-2.6.21.x/drivers/net/pppoe.c
@@ -440,13 +440,13 @@ static int pppoe_disc_rcv(struct sk_buff *skb,
 	if (ph->code != PADT_CODE)
 		goto abort;
 
+	/* Ignore PADT packets whose destination address isn't ours */
+	if (memcmp(eth_hdr(skb)->h_dest, dev->dev_addr, ETH_ALEN))
+		goto abort;
+
 	po = get_item((unsigned long) ph->sid, eth_hdr(skb)->h_source, dev->ifindex);
 	if (po) {
 		struct sock *sk = sk_pppox(po);
-
-		/* Check destination address */
-		if (memcmp(eth_hdr(skb)->h_dest, dev->dev_addr, ETH_ALEN))
-			goto abort;
 
 		bh_lock_sock(sk);
 
