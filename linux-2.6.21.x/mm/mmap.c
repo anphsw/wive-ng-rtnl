b19d40ce4de77381ae03cd1ac268d883efad62fe
sfstudio <sfstudio@mail.ru>
2010-12-01 14:04:31 +0600

    Remove unused variable in get_unmapped_area

--- linux-2.6.21.x/mm/mmap.c
+++ linux-2.6.21.x/mm/mmap.c
@@ -1365,7 +1365,6 @@ unsigned long
 get_unmapped_area(struct file *file, unsigned long addr, unsigned long len,
 		unsigned long pgoff, unsigned long flags)
 {
-	unsigned long ret;
 	unsigned long (*get_area)(struct file *, unsigned long,
 				  unsigned long, unsigned long, unsigned long);
 
