aef8f5d73ac4429002b07dee4a2b157e180bfe3b
sfstudio <sfstudio@mail.ru>
2010-12-07 19:14:36 +0600

    Drop support kernel version <2.6.21

--- linux-2.6.21.x/drivers/net/wireless/rt2860v2/os/linux/rt_linux.c
+++ linux-2.6.21.x/drivers/net/wireless/rt2860v2/os/linux/rt_linux.c
@@ -1357,15 +1357,11 @@ int RtmpOSIRQRequest(IN PNET_DEV pNetDev)
 			printk("RT2860: request_irq  ERROR(%d)\n", retval);
 	}
 #endif // RTMP_PCI_SUPPORT //
-
 #ifdef RTMP_RBUS_SUPPORT
 	if (pAd->infType == RTMP_DEV_INF_RBUS)
 	{
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,22)
-		if ((retval = request_irq(net_dev->irq, rt2860_interrupt, IRQF_SHARED, net_dev->name ,net_dev))) 
-#else
-		if ((retval = request_irq(net_dev->irq,rt2860_interrupt, SA_INTERRUPT, net_dev->name ,net_dev))) 
-#endif
+		retval = request_irq(net_dev->irq, rt2860_interrupt, IRQF_SHARED, (net_dev)->name ,(net_dev));
+		if (retval != 0) 
 			printk("RT2860: request_irq  ERROR(%d)\n", retval);
 	}
 #endif // RTMP_RBUS_SUPPORT //
