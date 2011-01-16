ac62c1bc01f6e1290d59c5c2780612d2c9687777
sfstudio <sfstudio@mail.ru>
2011-01-17 02:12:52 +0600

    Move to 4.5.2 gcc based toolchain.

--- linux-2.6.21.x/drivers/net/wireless/rt2860v2/common/mlme.c
+++ linux-2.6.21.x/drivers/net/wireless/rt2860v2/common/mlme.c
@@ -6451,7 +6451,7 @@ ULONG BssTableSetEntry(
 					BssEntrySet(pAd, &Tab->BssEntry[Idx], pBssid, Ssid, SsidLen, BssType, BeaconPeriod, CfParm, AtimWin, 
 						CapabilityInfo, SupRate, SupRateLen, ExtRate, ExtRateLen,pHtCapability, pAddHtInfo,HtCapabilityLen, AddHtInfoLen,
 						NewExtChanOffset, ChannelNo, Rssi, TimeStamp, CkipFlag, pEdcaParm, pQosCapability, pQbssLoad, LengthVIE, pVIE);
-                    Tab->BssOverlapNr = (Tab->BssOverlapNr++) % MAX_LEN_OF_BSS_TABLE;
+                			Tab->BssOverlapNr = ((Tab->BssOverlapNr++) % MAX_LEN_OF_BSS_TABLE);
 				}
 				return Idx;
 			}
