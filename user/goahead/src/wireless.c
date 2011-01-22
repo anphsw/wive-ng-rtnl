12a43cba32faeb885f8677267549e80cb08753cd
sfstudio <sfstudio@mail.ru>
2011-01-22 22:09:30 +0600

    Try fix crash at stalist table get.

--- user/goahead/src/wireless.c
+++ user/goahead/src/wireless.c
@@ -19,6 +19,7 @@
 #include	"wireless.h"
 #include	"internet.h"
 #include	"oid.h"
+#include	"stapriv.h"
 #include 	"wps.h"
 
 #define AP_MODE
@@ -272,38 +273,6 @@ static int getWlanCurrentMac(int eid, webs_t wp, int argc, char_t **argv)
 		return websWrite(wp, T(" "));
 	return websWrite(wp, T("%s"), if_hw);
 }
-
-typedef union _MACHTTRANSMIT_SETTING {
-	struct  {
-		unsigned short  MCS:7;  // MCS
-		unsigned short  BW:1;   //channel bandwidth 20MHz or 40 MHz
-		unsigned short  ShortGI:1;
-		unsigned short  STBC:2; //SPACE
-		unsigned short  rsv:3;
-		unsigned short  MODE:2; // Use definition MODE_xxx.
-	} field;
-	unsigned short      word;
-} MACHTTRANSMIT_SETTING;
-
-typedef struct _RT_802_11_MAC_ENTRY {
-    unsigned char       	Addr[6];
-    unsigned char       	Aid;
-    unsigned char       	Psm;     // 0:PWR_ACTIVE, 1:PWR_SAVE
-    unsigned char               MimoPs;  // 0:MMPS_STATIC, 1:MMPS_DYNAMIC, 3:MMPS_Enabled
-    char                	AvgRssi0;
-    char            		AvgRssi1;
-    char            		AvgRssi2;
-    unsigned int          	ConnectedTime;
-    MACHTTRANSMIT_SETTING       TxRate;
-    unsigned int          	LastRxRate;
-    int           		StreamSnr[3];
-    int           		SoundingRespSnr[3];
-} RT_802_11_MAC_ENTRY;
-
-typedef struct _RT_802_11_MAC_TABLE {
-	unsigned long            Num;
-	RT_802_11_MAC_ENTRY      Entry[32]; //MAX_LEN_OF_MAC_TABLE = 32
-} RT_802_11_MAC_TABLE;
 
 static int getWlanStaInfo(int eid, webs_t wp, int argc, char_t **argv)
 {
