1aaaa0e392b974bed52d02553a9767d0a9b1ca2a
sfstudio <sfstudio@mail.ru>
2011-01-23 09:55:11 +0600

    [PATCH] kernel.h: add {min,max}3 macros, replace nested max/min macros with {max,min}3 macro

--- linux-2.6.21.x/include/linux/kernel.h
+++ linux-2.6.21.x/include/linux/kernel.h
@@ -334,6 +334,24 @@ static inline char *pack_hex_byte(char *buf, u8 byte)
 	({ type __x = (x); type __y = (y); __x > __y ? __x: __y; })
 
 
+#define min3(x, y, z) ({			\
+	typeof(x) _min1 = (x);			\
+	typeof(y) _min2 = (y);			\
+	typeof(z) _min3 = (z);			\
+	(void) (&_min1 == &_min2);		\
+	(void) (&_min1 == &_min3);		\
+	_min1 < _min2 ? (_min1 < _min3 ? _min1 : _min3) : \
+		(_min2 < _min3 ? _min2 : _min3); })
+
+#define max3(x, y, z) ({			\
+	typeof(x) _max1 = (x);			\
+	typeof(y) _max2 = (y);			\
+	typeof(z) _max3 = (z);			\
+	(void) (&_max1 == &_max2);		\
+	(void) (&_max1 == &_max3);		\
+	_max1 > _max2 ? (_max1 > _max3 ? _max1 : _max3) : \
+		(_max2 > _max3 ? _max2 : _max3); })
+
 /**
  * container_of - cast a member of a structure out to the containing structure
  * @ptr:	the pointer to the member.
