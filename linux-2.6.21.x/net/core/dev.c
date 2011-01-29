0acb8d2da3fef18833a264204fe0eb840e8ee499
sfstudio <sfstudio@mail.ru>
2011-01-29 08:03:22 +0600

    dev.c fix build.

--- linux-2.6.21.x/net/core/dev.c
+++ linux-2.6.21.x/net/core/dev.c
@@ -267,7 +267,7 @@ static inline struct list_head *ptype_head(const struct packet_type *pt)
 	if (pt->type == htons(ETH_P_ALL))
 		return &ptype_all;
 	else
-		return &ptype_base[ntohs(pt->type) & 15;];
+		return &ptype_base[ntohs(pt->type) & 15];
 }
 
 /**
