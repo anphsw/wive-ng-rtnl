0b40070a2b80f18c79c15275aa3f2a329d056956
sfstudio <sfstudio@mail.ru>
2011-02-02 17:56:10 +0600

    Revert WRT EAP patch. This patch break compat with some windows drivers atheros and ralink in WPA mode. Fucked windows programmers..

--- linux-2.6.21.x/net/bridge/br_input.c
+++ linux-2.6.21.x/net/bridge/br_input.c
@@ -112,11 +112,7 @@ int br_handle_frame_finish(struct sk_buff *skb)
 
 	dst = NULL;
 
-	if (skb->protocol == htons(ETH_P_PAE)) {
-		skb2 = skb;
-		/* Do not forward 802.1x/EAP frames */
-		skb = NULL;
-	} else if (is_multicast_ether_addr(dest)) {
+	if (is_multicast_ether_addr(dest)) { 
 #ifdef CONFIG_BRIDGE_IGMPP_PROCFS
 		spin_lock_bh(&br->lock); // bridge lock
 
