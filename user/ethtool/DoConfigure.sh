4bf42f26c36bc07d3e8a17fe283d8620d7d56d0e
sfstudio <sfstudio@mail.ru>
2011-02-07 23:12:32 +0600

    Merge and builds fixups...

--- user/ethtool/DoConfigure.sh
+++ user/ethtool/DoConfigure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 APROOTDIR=`pwd`
-CONFOPTS="--host=mipsel-linux --prefix=$APROOTDIR/filesystem"
+CONFOPTS="--host=mipsel-linux --prefix=$APROOTDIR/filesystem --disable-dependency-tracking"
 ./configure $CONFOPTS
--- user/wsc_upnp/libupnp-1.4.6/DoConfigure.sh
+++ user/wsc_upnp/libupnp-1.4.6/DoConfigure.sh
@@ -5,5 +5,5 @@ APROOTDIR=`pwd`
 #to stop reconfigure fix me
 touch *
 
-CONFOPTS="--host=mipsel-linux --prefix=$APROOTDIR/filesystem"
+CONFOPTS="--host=mipsel-linux --prefix=$APROOTDIR/filesystem --disable-dependency-tracking"
 ./configure $CONFOPTS
