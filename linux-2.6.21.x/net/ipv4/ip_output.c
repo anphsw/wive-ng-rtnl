3c6525b1de5b7d474f4423531ac2f1834f6b35b9
sfstudio <sfstudio@mail.ru>
2011-02-28 01:25:43 +0600

    Merge...

--- linux-2.6.21.x/net/ipv4/ip_output.c
+++ linux-2.6.21.x/net/ipv4/ip_output.c
@@ -407,6 +407,7 @@ static void ip_copy_metadata(struct sk_buff *to, struct sk_buff *from)
 	nf_bridge_put(to->nf_bridge);
 	to->nf_bridge = from->nf_bridge;
 	nf_bridge_get(to->nf_bridge);
+
 #endif
 #endif
 	skb_copy_secmark(to, from);
