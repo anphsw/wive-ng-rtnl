4eb31b2fedcc74919dc46ae51cabd40bd27f9e6f
sfstudio <sfstudio@mail.ru>
2011-04-19 03:12:33 +0700

    Revert Dlink workaround.

--- etc/scripts/global.sh
+++ etc/scripts/global.sh
@@ -242,13 +242,9 @@ resetPhy()
 if [ "$CONFIG_RT_3052_ESW" = "y" ]; then
     $LOG "Reinit power mode for all switch ports"
     wan_port=`nvram_get 2860 wan_port`
-    if [ "$wan_port" = "0" ]; then
-	#workaroud for dir-300NRU
-	config-vlan.sh 2 FFFFF
-    else
-	#only link up/down
-	config-vlan.sh 2 RRRRR
-    fi
+    # workaroud for dir-300NRU and some devices
+    # with not correct configured from uboot
+    config-vlan.sh 2 FFFFF
 fi
 }
 
