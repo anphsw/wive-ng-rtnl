82b8bfe5a989774a7b5486dd2fc484351f197815
sfstudio <sfstudio@mail.ru>
2011-05-18 04:52:42 +0700

    Merge...

--- user/zebra/DoConfigure.sh
+++ user/zebra/DoConfigure.sh
@@ -2,5 +2,5 @@
 
 APROOTDIR=`pwd`
 CONFOPTS="--host=mipsel-linux"
-CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem --disable-ipv6 --disable-bgpd --disable-ripngd --disable-ospfd --disable-ospf6d"
+CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem --disable-ipv6 --disable-bgpd --disable-ripngd --disable-ospfd --disable-ospf6d --disable-dependency-tracking"
 ./configure $CONFOPTS
