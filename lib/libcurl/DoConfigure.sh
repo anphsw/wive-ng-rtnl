b7516741a9fe9201b71bb6a1a285a65919525ba9
sfstudio <sfstudio@mail.ru>
2011-05-18 01:35:07 +0700

    Tune configure options for curl.

--- lib/libcurl/DoConfigure.sh
+++ lib/libcurl/DoConfigure.sh
@@ -8,7 +8,7 @@ BACKUPLDFLAGS=$LDFLAGS
 LIBSSL=$LIBDIR/libssl
 ZLIB=$LIBDIR/zlib
 
-CONFOPTS="--host=mipsel-linux --build=i686-pc-linux-gnu --without-random --without-ssl" 
+CONFOPTS="--host=mipsel-linux --build=i686-pc-linux-gnu --without-random --without-ssl --disable-debug --disable-curldebug --disable-dependency-tracking" 
 CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem"
 CFLAGS="$BACKUPCFLAGS -I$ZLIB -I$LIBSSL" 
 LDFLAGS="$BACKUPLDFLAGS -L$ZLIB -L$LIBSSL"
