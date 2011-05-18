779d70d4444f968db155df74a5280d35b9c7f5b6
sfstudio <sfstudio@mail.ru>
2011-05-18 09:37:38 +0700

    transmission build tune.

--- user/transmission/DoConfigure.sh
+++ user/transmission/DoConfigure.sh
@@ -5,9 +5,10 @@ APROOTDIR=`pwd`
 PREFIX="--prefix=$APROOTDIR/filesystem"
 CONFOPTS="--host=mipsel-linux --build=i686-pc-linux-gnu \
 	    $PREFIX --disable-gtk --disable-nls --enable-lightweight --disable-utp \
+	    --disable-dependency-tracking \
 	    LIBCURL_LIBS=/opt/Wive-RTNL/lib/libcurl/filesystem/lib/libcurl.so \
 	    LIBEVENT_LIBS=/opt/Wive-RTNL/lib/libevent/filesystem/lib/libevent.so \
-	    OPENSSL_LIBS=/opt/Wive-RTNL/user/openssl-0.9.8e/libcrypto.a"
-#	    --with-inotify --disable-dependency-tracking"
+	    OPENSSL_LIBS=/opt/Wive-RTNL/user/openssl/libcrypto.so.0.9.8 /opt/Wive-RTNL/user/openssl/libssl.so.0.9.8"
+#	    --with-inotify"
 
 ./configure $CONFOPTS
