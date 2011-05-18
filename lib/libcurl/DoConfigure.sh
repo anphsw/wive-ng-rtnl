2336538e81a2d58cdfe12e402d8dd77472501d85
sfstudio <sfstudio@mail.ru>
2011-05-18 22:36:24 +0700

    Fix pcap build.

--- lib/libcurl/DoConfigure.sh
+++ lib/libcurl/DoConfigure.sh
@@ -5,8 +5,8 @@ APROOTDIR=`pwd`
 LIBDIR=$FIRMROOT/lib
 BACKUPCFLAGS=$CFLAGS
 BACKUPLDFLAGS=$LDFLAGS
-LIBSSL=$LIBDIR/libssl
 ZLIB=$LIBDIR/zlib
+LIBSSL=$FIRMROOT/user/openssl
 
 CONFOPTS="--host=mipsel-linux --build=i686-pc-linux-gnu --without-random --without-ssl --disable-debug --disable-curldebug --disable-dependency-tracking" 
 CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem"
