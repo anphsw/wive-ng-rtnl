5d9e1c682220cb04aa47402f5a27c1da24c61460
sfstudio <sfstudio@mail.ru>
2011-05-30 22:28:59 +0700

    net: Kill ether_table[] declaration. Not used from 2006y

--- linux-2.6.21.x/include/linux/if_ether.h
+++ linux-2.6.21.x/include/linux/if_ether.h
@@ -118,9 +118,6 @@ static inline struct ethhdr *eth_hdr(const struct sk_buff *skb)
 	return (struct ethhdr *)skb_mac_header(skb);
 }
 
-#ifdef CONFIG_SYSCTL
-extern struct ctl_table ether_table[];
-#endif
 #endif
 
 #endif	/* _LINUX_IF_ETHER_H */
