3dd03567402efbacd6e8fb2ccbad2ba4a619f0d4
sfstudio <sfstudio@mail.ru>
2011-05-31 21:55:55 +0700

    Flash API always use mtd char device.

--- lib/libnvram/flash_api.c
+++ lib/libnvram/flash_api.c
@@ -35,14 +35,10 @@ int mtd_open(const char *name, int flags)
 	if ((fp = fopen("/proc/mtd", "r"))) {
 		while (fgets(dev, sizeof(dev), fp)) {
 			if (sscanf(dev, "mtd%d:", &i) && strstr(dev, name)) {
-#ifdef CONFIG_MTD_BLOCK
-				snprintf(dev, sizeof(dev), "/dev/mtdblock%d", i);
-#else
-				snprintf(dev, sizeof(dev), "/dev/mtd/%d", i);
-#endif
+				snprintf(dev, sizeof(dev), "/dev/mtd%d", i);
 				ret = open(dev, flags);
 				if (ret < 0) {
-					snprintf(dev, sizeof(dev), "/dev/mtd%d", i);
+					snprintf(dev, sizeof(dev), "/dev/mtd/%d", i);
 					ret = open(dev, flags);
 				}
 				fclose(fp);
