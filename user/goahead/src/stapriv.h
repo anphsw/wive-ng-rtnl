82c4f34da686236c11461ca6bd32d091eea4e00b
sfstudio <sfstudio@mail.ru>
2011-06-15 21:30:10 +0700

    stapriv.h oops small revert.

--- user/goahead/src/stapriv.h
+++ user/goahead/src/stapriv.h
@@ -536,5 +536,35 @@ typedef struct _NDIS_802_11_PASSPHRASE
 	unsigned char           KeyMaterial[1];     // variable length depending on above field
 } NDIS_802_11_PASSPHRASE, *PNDIS_802_11_PASSPHRASE;
 
+typedef union _MACHTTRANSMIT_SETTING {
+	struct  {
+		unsigned short  MCS:7;  // MCS
+		unsigned short  BW:1;   //channel bandwidth 20MHz or 40 MHz
+		unsigned short  ShortGI:1;
+		unsigned short  STBC:2; //SPACE
+		unsigned short  rsv:3;
+		unsigned short  MODE:2; // Use definition MODE_xxx.
+	} field;
+	unsigned short      word;
+} MACHTTRANSMIT_SETTING;
 
+typedef struct _RT_802_11_MAC_ENTRY {
+    unsigned char		ApIdx;
+    unsigned char       	Addr[6];
+    unsigned char       	Aid;
+    unsigned char       	Psm;     // 0:PWR_ACTIVE, 1:PWR_SAVE
+    unsigned char               MimoPs;  // 0:MMPS_STATIC, 1:MMPS_DYNAMIC, 3:MMPS_Enabled
+    char                	AvgRssi0;
+    char            		AvgRssi1;
+    char            		AvgRssi2;
+    unsigned int          	ConnectedTime;
+    MACHTTRANSMIT_SETTING       TxRate;
+    unsigned int          	LastRxRate;
+    int           		StreamSnr[3];
+    int           		SoundingRespSnr[3];
+} RT_802_11_MAC_ENTRY;
 
+typedef struct _RT_802_11_MAC_TABLE {
+	unsigned long            Num;
+	RT_802_11_MAC_ENTRY      Entry[32]; //MAX_LEN_OF_MAC_TABLE = 32
+} RT_802_11_MAC_TABLE;
