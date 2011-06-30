7a1b9bb56df3c7d2d147f174406f55b4e257ad22
sfstudio <sfstudio@mail.ru>
2011-07-01 03:20:54 +0700

    Update transmission to 2.23 version.

--- user/transmission/DoConfigure.sh
+++ user/transmission/DoConfigure.sh
@@ -6,10 +6,13 @@ APROOTDIR=`pwd`
 ./configure --host=mipsel-linux --build=i686-pc-linux-gnu \
 	    --prefix=$APROOTDIR/filesystem --disable-gtk \
 	    --disable-nls --enable-lightweight --disable-utp \
-	    --disable-dependency-tracking --with-inotify \
+	    --enable-libnotify \
+	    --with-zlib=$FIRMROOT/lib/zlib/ \
+	    --with-zlib-includes=$FIRMROOT/lib/zlib/ \
 	    LIBCURL_CFLAGS="-I$FIRMROOT/lib/libcurl/include" \
 	    LIBCURL_LIBS="-L$FIRMROOT/lib/libcurl/filesystem/lib -lcurl" \
 	    LIBEVENT_CFLAGS="-I$FIRMROOT/lib/libevent/include" \
 	    LIBEVENT_LIBS="-L$FIRMROOT/lib/libevent/filesystem/lib -levent" \
 	    OPENSSL_CFLAGS="-I$FIRMROOT/user/openssl/include" \
-	    OPENSSL_LIBS="-L$FIRMROOT/user/openssl -lcrypto -lssl"
+	    OPENSSL_LIBS="-L$FIRMROOT/user/openssl -lcrypto -lssl" \
+	    --disable-dependency-tracking
