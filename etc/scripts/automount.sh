4d7a5dcd8d1fc248a83a342fb275e2f1152cda80
sfstudio <sfstudio@mail.ru>
2011-07-11 22:53:52 +0700

    dini: add hotplug logic for HDD/Flash/Modems support.

--- etc/scripts/automount.sh
+++ etc/scripts/automount.sh
@@ -1,45 +1,61 @@
-#! /bin/sh
-
-if [ "$1" == "" ]; then
-    echo "parameter is none" 
-    exit 1
-fi
-
-#load supportted FS modeules
-modprobe -q ext2
-modprobe -q ext3
-modprobe -q fuse
-modprobe -q udf
-
-mounted=`mount | grep $1 | wc -l`
-# mounted, assume we umount
-if [ $mounted -ge 1 ]; then
-    echo "R/media/$1" 
-    echo "R/media/$1" 
-    if ! umount "/media/$1"; then
-	exit 1
+#!/bin/sh
+if [ "$ACTION" = "add" ] ; then
+    if [ -f /bin/blkid ]; then
+	eval "$(blkid /dev/$MDEV | sed 's/^[^ ]* //;s/\([^ ]*=\)/MDEV_\1/g')"
+	readonly MDEV_TYPE MDEV_UUID MDEV_LABEL MDEV_PATH=/dev/$MDEV
+	line=`cat /etc/fstab | egrep -m 1 "^(UUID=${MDEV_UUID}|LABEL=${MDEV_LABEL}|${MDEV_PATH})[ \t]"`
+	if [ -z "$line" ]; then
+	    MOUNT_SRC=$MDEV_PATH
+	    if [ -z "$MDEV_LABEL" ]; then
+		MOUNT_DST="/media/$MDEV_UUID"
+	    else
+		MOUNT_DST="/media/$MDEV_LABEL"
+	    fi
+	else
+	    MOUNT_SRC=`echo $line | sed 's/ .*//'`
+	    if echo "$line" | egrep -q '[, \t]noauto[, \t]'; then
+		#no auto mounting
+		exit 0
+	    fi
+	    MOUNT_DST=`echo $line | sed 's/^[^ \t]*[ \t]*//;s/[ \t].*//'`
+	fi
+	if [ -d "$MOUNT_DST" ]; then
+	    [ -z "$line" ] && exit 1
+	else
+	    if ! mkdir -p "$MOUNT_DST"; then
+		exit 1
+	    fi
+	fi
+	if ! mount "$MOUNT_SRC" "$MOUNT_DST"; then
+	    if ! ntfs-3g "$MOUNT_SRC" "$MOUNT_DST" -o force; then
+		if ! rm -r "$MOUNT_DST"; then
+		    exit 1
+		fi
+	    fi
+	fi
+    else
+	if ! mkdir -p "/media/$MDEV"; then
+	    exit 1
+	fi
+	if ! mount "/dev/$MDEV" "/media/$MDEV"; then
+	    if ! ntfs-3g "/dev/$MDEV" "/media/$MDEV" -o force; then
+		if ! rm -r "/media/$MDEV"; then
+		    exit 1
+		fi
+	    fi
+	fi
     fi
-    if ! rm -r "/media/$1"; then
-	exit 1
-    fi
-# not mounted, lets mount under /media
 else
-    if ! mkdir -p "/media/$1"; then
-    exit 1
-fi
-
-if ! mount "/dev/$1" "/media/$1"; then
-    # failed to mount, clean up mountpoint
-    if ! ntfs-3g "/dev/$1" "/media/$1" -o force; then
-	if ! rm -r "/media/$1"; then
+    mount_point=`mount | grep $MDEV | awk '{print $3}'`
+    if [ `mount | grep $MDEV | wc -l` -ge 1 ]; then
+	if ! umount "/dev/$MDEV"; then
+	    exit 1
+	fi
+	if ! rm -r "$mount_point"; then
 	    exit 1
 	fi
     fi
 fi
-echo "A/media/$1" 
-echo "A/media/$1" 
-fi
 
-# Goahead need to know the event happened.
 killall -q -SIGTTIN goahead
 exit 0
