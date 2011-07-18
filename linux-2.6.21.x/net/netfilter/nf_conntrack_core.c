79ac6bd5324d7361775c6b2b1d68672650674135
sfstudio <sfstudio@mail.ru>
2011-07-19 04:04:40 +0700

    nf_conntrack_core.c oops defines fix...

--- linux-2.6.21.x/net/netfilter/nf_conntrack_core.c
+++ linux-2.6.21.x/net/netfilter/nf_conntrack_core.c
@@ -1272,7 +1272,7 @@ nf_conntrack_in(int pf, unsigned int hooknum, struct sk_buff **pskb)
 		}
 	    }
 #endif /* XT_MATCH_WEBSTR */
-//#if defined(CONFIG_TUN) || defined(CONFIG_TUN_MODULE)
+#if defined(CONFIG_TUN) || defined(CONFIG_TUN_MODULE)
 	    /* Skip tunnels traffic. Workaround for chillispot */
 	    if (strcmp(skb->dev->name, "tun")==0)
 #if  defined(CONFIG_RA_HW_NAT) || defined(CONFIG_RA_HW_NAT_MODULE)
@@ -1280,7 +1280,7 @@ nf_conntrack_in(int pf, unsigned int hooknum, struct sk_buff **pskb)
 #else
 			goto skip_sw;
 #endif
-//#endif /* TUN */
+#endif /* TUN */
 		/* Other traffic skip section
 		    .........
 		    .........
