cb80e74d8ea437debd1720a1dffa63f66b301372
sfstudio <sfstudio@mail.ru>
2011-08-09 04:54:21 +0700

    transmission: fix build from clean clone tree.

--- user/transmission/libtransmission/version.h
+++ user/transmission/libtransmission/version.h
@@ -1,9 +1,9 @@
 #define PEERID_PREFIX             "-TR2330-"
 #define USERAGENT_PREFIX          "2.33"
-#define SVN_REVISION              "12565"
-#define SVN_REVISION_NUM          12565
+#define SVN_REVISION              "12563"
+#define SVN_REVISION_NUM          12563
 #define SHORT_VERSION_STRING      "2.33"
-#define LONG_VERSION_STRING       "2.33 (12565)"
+#define LONG_VERSION_STRING       "2.33 (12563)"
 #define VERSION_STRING_INFOPLIST  2.33
 #define MAJOR_VERSION             2
 #define MINOR_VERSION             33
