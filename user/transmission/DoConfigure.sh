499df806a7d2501d30319cf6b9dc3a8f702e48d5
sfstudio <sfstudio@mail.ru>
2011-08-09 02:25:09 +0700

    transmission: DoConfigure.sh remove dead flags.

--- user/transmission/DoConfigure.sh
+++ user/transmission/DoConfigure.sh
@@ -15,12 +15,10 @@ fi
 	    --prefix=$APROOTDIR/filesystem --disable-gtk \
 	    --disable-nls --enable-lightweight --disable-utp \
 	    --enable-libnotify \
-	    --with-zlib=$FIRMROOT/lib/zlib/ \
-	    --with-zlib-includes=$FIRMROOT/lib/zlib/ \
+	    OPENSSL_CFLAGS="-I$FIRMROOT/user/openssl/include" \
+	    OPENSSL_LIBS="-L$FIRMROOT/user/openssl -lcrypto -lssl" \
 	    LIBCURL_CFLAGS="-I$FIRMROOT/lib/libcurl/include" \
 	    LIBCURL_LIBS="-L$FIRMROOT/lib/libcurl/filesystem/lib -lcurl" \
 	    LIBEVENT_CFLAGS="-I$FIRMROOT/lib/libevent/include" \
 	    LIBEVENT_LIBS="-L$FIRMROOT/lib/libevent/filesystem/lib -levent" \
-	    OPENSSL_CFLAGS="-I$FIRMROOT/user/openssl/include" \
-	    OPENSSL_LIBS="-L$FIRMROOT/user/openssl -lcrypto -lssl" \
 	    --disable-dependency-tracking
