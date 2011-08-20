5e2969bec9d6b4a7ac1937f26530ec05240a6bef
sfstudio <sfstudio@mail.ru>
2011-08-21 03:52:33 +0700

    strip.sh fix .* regexp.

--- strip.sh
+++ strip.sh
@@ -23,8 +23,8 @@ done
 chmod 777 $RO_ROOT/etc/scripts/config.sh
 
 echo -------------------------------FIND FILES TO STRIP-----------------------------
-NON_STRIPS_BIN=`find $RO_ROOT/bin -type f -print -exec file {} \; | grep -v "modules" | grep -v "icon" | grep -v "start" | grep -v "rc" | grep -v ".sh" | cut -d":" -f1`
-NON_STRIPS_LIB=`find $RO_ROOT/lib -type f -print -exec file {} \; | grep -v "modules" | grep -v ".a" | grep -v ".la" | grep -v ".pc" | cut -d":" -f1`
+NON_STRIPS_BIN=`find $RO_ROOT/bin -type f -print -exec file {} \; | grep -v "modules" | grep -v "icon" | grep -v "start" | grep -v "rc" | grep -v "\.sh" | cut -d":" -f1`
+NON_STRIPS_LIB=`find $RO_ROOT/lib -type f -print -exec file {} \; | grep -v "modules" | grep -v "\.a" | grep -v "\.la" | grep -v "\.pc" | cut -d":" -f1`
 echo -----------------------------------STRIP BIN----------------------------------
 for i in $NON_STRIPS_BIN; do
     echo $i;
