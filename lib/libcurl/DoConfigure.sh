b0c697dcd73d5cd3534027c8fea340331c26daa2
sfstudio <sfstudio@mail.ru>
2011-08-20 10:18:01 +0700

    tune curl configure for decrease size.

--- lib/libcurl/DoConfigure.sh
+++ lib/libcurl/DoConfigure.sh
@@ -16,7 +16,9 @@ if [ ! -f $APROOTDIR/Makefile.in ]; then
     automake
 fi
 
-CONFOPTS="--host=mipsel-linux --build=i686-pc-linux-gnu --without-random --without-ssl --disable-debug --disable-curldebug --disable-dependency-tracking" 
+CONFOPTS="--host=mipsel-linux --build=i686-pc-linux-gnu --without-random "
+CONFOPTS="$CONFOPTS --without-ssl --disable-debug --disable-curldebug"
+CONFOPTS="$CONFOPTS --disable-dependency-tracking --disable-verbose --disable-rtsp"
 CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem"
 CFLAGS="$BACKUPCFLAGS -I$ZLIB -I$LIBSSL" 
 LDFLAGS="$BACKUPLDFLAGS -L$ZLIB -L$LIBSSL"
