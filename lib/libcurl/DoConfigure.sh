8072b0040255c2d5f304a77cfe590aab86131682
sfstudio <sfstudio@mail.ru>
2011-08-22 10:45:16 +0700

    libcurl disable manuals build.

--- lib/libcurl/DoConfigure.sh
+++ lib/libcurl/DoConfigure.sh
@@ -17,7 +17,7 @@ if [ ! -f $APROOTDIR/Makefile.in ]; then
 fi
 
 CONFOPTS="--host=mipsel-linux --build=i686-pc-linux-gnu --without-random "
-CONFOPTS="$CONFOPTS --without-ssl --disable-debug --disable-curldebug"
+CONFOPTS="$CONFOPTS --without-ssl --disable-debug --disable-curldebug --disable-manual"
 CONFOPTS="$CONFOPTS --disable-dependency-tracking --disable-verbose --disable-rtsp"
 CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem"
 CFLAGS="$BACKUPCFLAGS -I$ZLIB -I$LIBSSL" 
