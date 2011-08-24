3725d907b6c5d4fa99fad4191ac9673f7d2e2f52
sfstudio <sfstudio@mail.ru>
2011-08-24 23:08:47 +0700

    samba3: DoConfigure.sh some change.

--- user/samba3/DoConfigure.sh
+++ user/samba3/DoConfigure.sh
@@ -9,15 +9,20 @@ if [ ! -f configure ]; then
     sh ./autogen.sh
 fi
 
+#arch options
 CONFOPTS="--host=mipsel-linux --build=i686-pc-linux-gnu --target=mipsel"
+#disable some function
 CONFOPTS="$CONFOPTS --disable-iprint --disable-pie --disable-relro --disable-fam --disable-cups --disable-debug"
 CONFOPTS="$CONFOPTS --disable-dnssd --disable-avahi --disable-swat --disable-dmalloc --disable-nss-wrapper"
 CONFOPTS="$CONFOPTS --disable-developer --disable-krb5developer --disable-picky-developer"
 CONFOPTS="$CONFOPTS --without-ldap --without-ads --without-cifsmount"
-CONFOPTS="$CONFOPTS --without-cifsupcall --with-syslog --without-sys-quotas"
+CONFOPTS="$CONFOPTS --without-cifsupcall --without-sys-quotas"
 CONFOPTS="$CONFOPTS --without-utmp --without-cluster-support --without-winbind"
 CONFOPTS="$CONFOPTS --without-libtalloc --without-libtdb --without-libnetapi"
 CONFOPTS="$CONFOPTS --without-libsmbclient --without-libsmbsharemodes --without-libaddns --without-sendfile-support"
+#some function direct enable
+CONFOPTS="$CONFOPTS --with-syslog --enable-largefile"
+#path options
 CONFOPTS="$CONFOPTS --with-logfilebase=/var/log --with-piddir=/var/lock --with-lockdir=/var/lock --sysconfdir=/etc"
 CONFOPTS="$CONFOPTS --with-libiconv=$FIRMROOT/lib/lib $FIRMROOT/lib/libiconv/include --prefix=/"
 
