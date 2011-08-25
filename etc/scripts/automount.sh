71dec0118ba2ef28537d1305aa26e420907b70e7
sfstudio <sfstudio@mail.ru>
2011-08-26 04:29:11 +0700

    dini: rewrite automount.sh support auto mount optware by label.

--- etc/scripts/automount.sh
+++ etc/scripts/automount.sh
@@ -1,88 +1,100 @@
 #!/bin/sh
 
-try_mount() {
-if ! mount -o utf8, noatime "$MOUNT_SRC" "$MOUNT_DST"; then
-    if ! mount -o noatime "$MOUNT_SRC" "$MOUNT_DST"; then
-	if ! ntfs-3g "$MOUNT_SRC" "$MOUNT_DST" -o force,noatime; then
-	    if ! rm -r "$MOUNT_DST"; then
-		exit 1
-	    fi
-	fi
+LOG="logger -t automount"
+MDEV_PATH=/dev/$MDEV
+
+pre_mount() {
+  if [ "$MDEV_LABEL" == "optware" ]; then
+    $LOG "optware part"
+    MOUNT_DST="/opt"
+  else
+    MOUNT_DST="/media/$MDEV"
+    if [ -d "$MOUNT_DST" ]; then
+      $LOG "dir $MOUNT_DST is exist"
+      exit 1
+    else
+      if ! mkdir -p "$MOUNT_DST"; then
+	$LOG "can not create $MOUNT_DST"
+	exit 1
+      fi
     fi
-fi
+  fi
 }
 
-try_umount() {
-mount_point=`mount | grep $MDEV | awk '{print $3}'`
-if [ `mount | grep $MDEV | wc -l` -ge 1 ]; then
-    sync
-    if ! umount "/dev/$MDEV"; then
-	if ! umount -l "/dev/$MDEV"; then
-	    exit 1
-	fi
+try_mount() {
+  modprobe -q "$MDEV_TYPE"
+# mount -o utf8 not worked need external mount, busybox not support this
+#  if ! mount -o utf8,noatime -t "$MDEV_TYPE" "$MDEV_PATH" "$MOUNT_DST"; then
+    if ! mount -o noatime -t "$MDEV_TYPE" "$MDEV_PATH" "$MOUNT_DST"; then
+      $LOG "can not mount"
+      mount_err
     fi
-    if ! rm -r "$mount_point"; then
-        exit 1
-    fi
-fi
+#  fi
+}
+
+try_ntfs() {
+  if ! ntfs-3g "$MDEV_PATH" "$MOUNT_DST" -o force,noatime; then
+    $LOG "can not mount NTFS"
+    mount_err
+  fi
 }
 
 swap_on() {
-auto_swap=`nvram_get 2860 auto_swap`
-if [ "$auto_swap" = "1" ] && [ -f /bin/swapon ] && [ -f /bin/fdisk ]; then
-    SWAPPART=`fdisk -l | grep swap | awk {' print $1 '}`
-    if [ "$SWAPPART" != "" ]; then
-	swapon $SWAPPART > /dev/null 2>&1
+  if [ "$(nvram_get 2860 auto_swap)" = "1" ] && [ -f /bin/swapon ]; then
+    $LOG "Swap on"
+    swapon "$MDEV_PATH" > /dev/null 2>&1
+  fi
+}
+
+try_umount() {
+  if [ `mount | grep "$MDEV" | wc -l` -ge 1 ]; then
+    $LOG "umount"
+    sync
+    if ! umount "$MDEV_PATH"; then
+      if ! umount -l "$MDEV_PATH"; then
+	$LOG "can not unmount"
+	exit 1
+      fi
     fi
-fi
+    MOUNT_DST="/media/$MDEV"
+    mount_err
+  fi
 }
 
 swap_off() {
-if [ -f /bin/swapoff ]; then
-    swapoff -a > /dev/null 2>&1
-fi
+  if [ `cat /proc/swaps | grep "$MDEV" | wc -l` -ge 1 ]; then
+    $LOG "swap off"
+    swapoff "$MDEV_PATH"
+  fi
 }
 
-if [ "$ACTION" = "add" ] ; then
-    swap_on
-    if [ -f /bin/blkid ]; then
-	eval "$(blkid /dev/$MDEV | sed 's/^[^ ]* //;s/\([^ ]*=\)/MDEV_\1/g')"
-	readonly MDEV_TYPE MDEV_UUID MDEV_LABEL MDEV_PATH=/dev/$MDEV
-	line=`cat /etc/fstab | egrep -m 1 "^(UUID=${MDEV_UUID}|LABEL=${MDEV_LABEL}|${MDEV_PATH})[ \t]"`
-	if [ -z "$line" ]; then
-	    MOUNT_SRC=$MDEV_PATH
-	    if [ -z "$MDEV_LABEL" ]; then
-		MOUNT_DST="/media/$MDEV_UUID"
-	    else
-		MOUNT_DST="/media/$MDEV_LABEL"
-	    fi
-	else
-	    MOUNT_SRC=`echo $line | sed 's/ .*//'`
-	    if echo "$line" | egrep -q '[, \t]noauto[, \t]'; then
-		#no auto mounting
-		exit 0
-	    fi
-	    MOUNT_DST=`echo $line | sed 's/^[^ \t]*[ \t]*//;s/[ \t].*//'`
-	fi
-	if [ -d "$MOUNT_DST" ]; then
-	    [ -z "$line" ] && exit 1
-	else
-	    if ! mkdir -p "$MOUNT_DST"; then
-		exit 1
-	    fi
-	fi
-	try_mount
-    else
-	MOUNT_SRC="/dev/$MDEV"
-	MOUNT_DST="/media/$MDEV"
-	if ! mkdir -p "$MOUNT_DST"; then
-	    exit 1
-	fi
-	try_mount
-    fi
+mount_err() {
+  if ! rm -r "$MOUNT_DST"; then
+    $LOG "can not del $MOUNT_DST"
+    exit 1
+  fi
+}
+
+if [ "$ACTION" = "add" ]; then
+  eval "$(blkid $MDEV_PATH | sed 's/^[^ ]* //;s/\([^ ]*=\)/MDEV_\1/g')"
+  $LOG "add $MDEV_PATH with $MDEV_TYPE"
+  case "$MDEV_TYPE" in
+    ntfs)
+      pre_mount
+      try_ntfs
+      ;;
+    swap)
+      swap_on
+      ;;
+    *)
+      pre_mount
+      try_mount
+      ;;
+  esac
 else
-    swap_off
-    try_umount
+  $LOG "remove $MDEV_PATH"
+  try_umount
+  swap_off
 fi
 
 killall -q -SIGTTIN goahead
