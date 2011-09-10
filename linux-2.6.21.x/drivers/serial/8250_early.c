4eb37197bd0890d3dc8f4eded1cfc9b20257dc30
sfstudio <sfstudio@mail.ru>
2011-09-10 16:49:21 +0700

    fix RT6855 support in serial drivers.

--- linux-2.6.21.x/drivers/serial/8250_early.c
+++ linux-2.6.21.x/drivers/serial/8250_early.c
@@ -107,6 +107,7 @@ static unsigned int __init probe_baud(struct uart_port *port)
     !defined (CONFIG_RALINK_RT3883) && \
     !defined (CONFIG_RALINK_RT3352) && \
     !defined (CONFIG_RALINK_RT3052) && \
+    !defined (CONFIG_RALINK_RT6855) && \
     !defined (CONFIG_RALINK_RT5350)
 	unsigned int dll=0, dlm=0;
 #endif
@@ -118,6 +119,7 @@ static unsigned int __init probe_baud(struct uart_port *port)
     defined (CONFIG_RALINK_RT3883) || \
     defined (CONFIG_RALINK_RT3352) || \
     defined (CONFIG_RALINK_RT3052) || \
+    defined (CONFIG_RALINK_RT6855) || \
     defined (CONFIG_RALINK_RT5350)
 	quot= serial_in(port, UART_DLL);
 #else
