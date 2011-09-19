057aa363014914c77cf370237996efec1ef85b95
sfstudio <sfstudio@mail.ru>
2011-09-19 09:52:03 +0700

    Rebuild 1.10.7. Disable carrier detect support - buggi.

--- etc/scripts/wifi.sh
+++ etc/scripts/wifi.sh
@@ -55,15 +55,6 @@ if [ "$CONFIG_RT2860V2_AP_IGMP_SNOOP" != "" ]; then
     fi
 fi
 
-###############################################Radar,DFS,CD########################
-CarrierDetect=`nvram_get CarrierDetect`
-if [ "$CarrierDetect" = "1" ]; then
-    iwpriv ra0 set CarrierDetect=1
-    iwpriv ra0 set RadarStart=1
-else
-    iwpriv ra0 set RadarStop=1
-    iwpriv ra0 set CarrierDetect=0
-fi
 ###############################################Channel select#######################
 AutoChannelSelect=`nvram_get AutoChannelSelect`
 if [ "$AutoChannelSelect" = "1" ]; then
