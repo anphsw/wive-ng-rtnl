bda989fe261c55f6a7f5ffa9ba2865e117a06587
sfstudio <sfstudio@mail.ru>
2011-09-22 04:28:19 +0700

    need reinit LAN port at switch for dhcp renew at clients

--- etc/scripts/services_restart.sh
+++ etc/scripts/services_restart.sh
@@ -71,5 +71,10 @@ fi
 if [ "$dnsPEnabled" != "1" ] && [ "$wan_static_dns" != "on" ]; then
     if [ "$MODE" = "pppd" ] || [ "$MODE" = "dhcp" ]; then 
 	service dhcpd restart
+	if [ "$CONFIG_RT_3052_ESW" != "" ]; then
+	    # need reinit LAN port at switch
+	    # for dhcp renew at clients
+	    config-vlan.sh 2 RRRR > /dev/null 2>&1
+	fi
     fi
 fi
