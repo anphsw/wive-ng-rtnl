7c6eec2f3021234bd12abed72006790367285f6e
sfstudio <sfstudio@mail.ru>
2011-09-23 16:44:50 +0700

    Printserver p910nd use bidirection mode by default.

--- user/usb/prnctrl.sh
+++ user/usb/prnctrl.sh
@@ -7,17 +7,19 @@ LOG="logger -t prnctrl"
 port=${MDEV##*lp}
 
 if [ "$ACTION" = "add" ]; then
-    if [ -z "$(iptables -L | grep "jetdirect")" ]; then
-	iptables -A servicelimit -i $lan_if -p tcp --dport 9100 -j ACCEPT > /dev/null 2>&1
-	$LOG "Add p910nd firewall rules"
-    fi
-    # For GDI printers put printers firmware file in /etc, rename to prnfw.dl
-    if [ -f /etc/prnfw.dl ]; then
-	cat prnfw.dl > /dev/usb/$MDEV
-    fi
     if [ -z "`pidof p910nd`" ]; then
+	# For GDI printers put printers firmware
+	# file in /etc, rename to prnfw.dl
+	if [ -f /etc/prnfw.dl ]; then
+	    $LOG "Upload firmware /etc/prnfw.dl to printer $MDEV."
+	    cat /etc/prnfw.dl > /dev/usb/$MDEV
+	fi
 	$LOG "Start p910nd daemon on port 910${port}"
 	/bin/p910nd -b -f /dev/usb/$MDEV $port
+	if [ -z "$(iptables -L | grep "jetdirect")" ]; then
+	    $LOG "Add p910nd firewall rules"
+	    iptables -A servicelimit -i $lan_if -p tcp --dport 9100 -j ACCEPT > /dev/null 2>&1
+	fi
     fi
 else
     if [ ! -z "`pidof p910nd`" ]; then
