b89bc5e92944e2ce1366e9d9b803450c2231026b
sfstudio <sfstudio@mail.ru>
2011-10-09 00:55:02 +0700

    udp4_lib_rcv fix path.

--- linux-2.6.21.x/net/ipv4/udp.c
+++ linux-2.6.21.x/net/ipv4/udp.c
@@ -1118,6 +1118,9 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct hlist_head udptable[],
 		goto drop;		/* No space for header. */
 
 	ulen = ntohs(uh->len);
+	saddr = ip_hdr(skb)->saddr;
+	daddr = ip_hdr(skb)->daddr;
+
 	if (ulen > skb->len)
 		goto short_packet;
 
@@ -1133,9 +1136,6 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct hlist_head udptable[],
 		if (udplite4_csum_init(skb, uh))
 			goto csum_error;
 	}
-
-	saddr = ip_hdr(skb)->saddr;
-	daddr = ip_hdr(skb)->daddr;
 
 	if(rt->rt_flags & (RTCF_BROADCAST|RTCF_MULTICAST))
 		return __udp4_lib_mcast_deliver(skb, uh, saddr, daddr, udptable);
