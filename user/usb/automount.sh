92ceb6215a5d22c62e3cb3b78b2b3bf58c933709
sfstudio <sfstudio@mail.ru>
2011-10-26 23:42:00 +0700

    automount.sh add transmission restart at disk mount.

--- user/usb/automount.sh
+++ user/usb/automount.sh
@@ -117,6 +117,7 @@ killall -q -SIGTTIN goahead
 if [ "$MDEV_LABEL" != "optware" ] && [ "$MDEV_TYPE" != "swap"]; then
     #regenerate config and restart samba
     service samba restart
+    service transmision restart
 fi
 
 exit 0
