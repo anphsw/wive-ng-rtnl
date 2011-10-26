8a891529c8e6bb3eceeee888c0bddbbfdac2c25e
sfstudio <sfstudio@mail.ru>
2011-10-26 20:56:57 +0700

    Merge...

--- user/samba/source/nmbd/asyncdns.c
+++ user/samba/source/nmbd/asyncdns.c
@@ -151,7 +151,7 @@ void start_async_dns(void)
 		fd_out = fd2[1];
 		close(fd1[1]);
 		close(fd2[0]);
-		DEBUG(0,("started asyncdns process %d\n", (int)child_pid));
+		//DEBUG(0,("started asyncdns process %d\n", (int)child_pid));
 		return;
 	}
 
