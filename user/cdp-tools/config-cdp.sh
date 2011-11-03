871ec4a8fdad758eecc4163bf1fa5151001ac6cb
sfstudio <sfstudio@mail.ru>
2011-11-03 22:49:47 +0700

    config-cdp.sh more readable version SW/HW for cdp. taf_321

--- user/cdp-tools/config-cdp.sh
+++ user/cdp-tools/config-cdp.sh
@@ -10,11 +10,9 @@ if [ "`pidof cdp-send`" ]; then
 fi
 if [ -f /bin/cdp-send ] && [ "$cdpEnabled" = "1" ]; then
     . /etc/scripts/global.sh
-    DEVNAME=`grep "DEVNAME" < /etc/version | awk {' print $3 '}`
-    VERSIONPKG=`grep "VERSIONPKG" < /etc/version | awk {' print $3 '}`
-    r_version="$DEVNAME-$VERSIONPKG"
+    DEVNAME=`grep "DEVNAME" < /etc/version | awk {' print $3 '} | sed s'@"@@'g`
+    VERSIONPKG=`grep "VERSIONPKG" < /etc/version | awk {' print $3 '} | sed s'@"@@'g`
     r_hostname=`hostname`
-    r_cap="l3r"
-    $LOG "Send CDP request mode $r_cap options $r_version $r_hostname $wan_if"
-    cdp-send -t 120 -c "$r_cap" -s "$r_version" -n "$r_hostname" "$wan_if" & > /dev/null 2>&1
+    $LOG "Send CDP request options $r_version $r_hostname $wan_if"
+    cdp-send -t 120 -m $DEVNAME -s $VERSIONPKG -n $r_hostname "$wan_if" & > /dev/null 2>&1
 fi
