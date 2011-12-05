2d881cf4d9393b2b18fd71b87129a84f2d064b12
sfstudio <sfstudio@mail.ru>
2011-12-05 19:55:28 +0700

    goahead: move command log to /var/log.

--- user/goahead/src/management.h
+++ user/goahead/src/management.h
@@ -4,17 +4,15 @@
 #define PROC_IF_STATISTIC	"/proc/net/dev"
 #define PROC_MEM_STATISTIC	"/proc/meminfo"
 
+#define SYSTEM_COMMAND_LOG	"/var/log/system_command.log"
 
 #define TXBYTE		0
 #define TXPACKET	1
 #define RXBYTE		2
 #define RXPACKET	3
 
-#define WIFI_STATUS "iwpriv ra0 stat"
-
-#define SYSTEM_COMMAND_LOG "/var/system_command.log"
-
 void formDefineManagement(void);
 void management_init(void);
 char *setNthValueLong(int index, char *old_values, char *new_value);
+
 #endif
