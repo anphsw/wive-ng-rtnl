599cfccda878415294ab810e78d317f36d3910ec
sfstudio <sfstudio@mail.ru>
2012-01-24 18:45:45 +0700

    SPI/NAND Fix restore firmware from emerge mode.

--- Uboot/common/cmd_tftpServer.c
+++ Uboot/common/cmd_tftpServer.c
@@ -464,10 +464,10 @@ static void SolveImage(void)
 		puts ("Copy to Flash... ");
 #if defined (CFG_ENV_IS_IN_NAND)
 		printf ("\n Copy %d byte to Flash... ", count*size);
-		rrc = ranand_erase_write((char *)CFG_LOAD_ADDR, 0, NetBootFileXferSize);
+		rrc = ranand_erase_write((uchar *)ptr, (ulong)(CFG_FLASH_BASE + 0x50000), count*size);
 #elif defined (CFG_ENV_IS_IN_SPI)
 		printf ("\n Copy %d byte to Flash... ", count*size);
-		rrc = raspi_erase_write((char *)CFG_LOAD_ADDR, 0, NetBootFileXferSize);
+		rrc = raspi_erase_write((uchar *)ptr, (ulong)(CFG_FLASH_BASE + 0x50000), count*size);
 #else /* NOR */
 		printf("\n Erase File System block !!\n From 0xBC450000 To 0xBC7FFFFF\n");
 		flash_sect_protect(0, CFG_FLASH_BASE + 0x50000, CFG_FLASH_BASE + 0x7fffff);
