b8d33cd0f6ec243e043b0e452bc27ae12a82aaa1
sfstudio <sfstudio@mail.ru>
2012-03-05 10:26:49 +0700

    WiFi drivers use native functions for check mcast/bcast mac adress.

--- linux-2.6.21.x/drivers/net/wireless/rt2860v2/common/igmp_snoop.c
+++ linux-2.6.21.x/drivers/net/wireless/rt2860v2/common/igmp_snoop.c
@@ -225,7 +225,7 @@ BOOLEAN MulticastFilterTableInsertEntry(
 		{
 			if (MAC_ADDR_EQUAL(pMemberAddr, pMemberEntry->Addr))
 			{
-				DBGPRINT(RT_DEBUG_ERROR, ("%s: already in Members list.\n", __FUNCTION__));
+				//DBGPRINT(RT_DEBUG_ERROR, ("%s: already in Members list.\n", __FUNCTION__));
 				return FALSE;
 			}
 
