2239ed632a2c19a20eca8a3c3ed4d42c2e7c85b7
sfstudio <sfstudio@mail.ru>
2012-03-13 21:10:27 +0700

    [NET] skbuff: remove export of static symbol

--- linux-2.6.21.x/net/core/skbuff.c
+++ linux-2.6.21.x/net/core/skbuff.c
@@ -2553,7 +2553,6 @@ EXPORT_SYMBOL(pskb_copy);
 EXPORT_SYMBOL(skb_checksum);
 EXPORT_SYMBOL(skb_morph);
 EXPORT_SYMBOL(skb_clone);
-EXPORT_SYMBOL(skb_clone_fraglist);
 EXPORT_SYMBOL(skb_copy);
 EXPORT_SYMBOL(skb_copy_and_csum_bits);
 EXPORT_SYMBOL(skb_copy_and_csum_dev);
