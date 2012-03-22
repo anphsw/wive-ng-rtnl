9170fd0b4eab606ff6a3ddd21a33e99cc2b0e37c
sfstudio <sfstudio@mail.ru>
2012-03-23 06:07:47 +0700

    try back to 4.4.x(4.4.7) gcc.

---
+++ toolchain/include/linux/compat_skbuff.h
@@ -0,0 +1,21 @@
+#ifndef COMPAT_SKBUFF_H
+#define COMPAT_SKBUFF_H 1
+
+struct tcphdr;
+struct udphdr;
+
+#define skb_ifindex(skb) (((skb)->input_dev != NULL) ? (skb)->input_dev->ifindex : 0)
+#define skb_nfmark(skb) (((struct sk_buff *)(skb))->mark)
+
+#ifdef CONFIG_NETWORK_SECMARK
+#	define skb_secmark(skb) ((skb)->secmark)
+#else
+#	define skb_secmark(skb) 0
+#endif
+
+#define ip_hdrlen(skb) (ip_hdr(skb)->ihl * 4)
+#define ipv6_hdr(skb) ((skb)->nh.ipv6h)
+#define skb_network_header(skb) ((skb)->nh.raw)
+#define skb_transport_header(skb) ((skb)->h.raw)
+#define skb_network_header_len(skb) (skb->h.raw - skb->nh.raw)
+#endif /* COMPAT_SKBUFF_H */
