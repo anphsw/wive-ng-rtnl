e78a8c7b12818716e68af4b4994f43d593659a34
sfstudio <sfstudio@mail.ru>
2012-04-24 00:57:07 +0700

    slab: Fix memory leak in fallback_alloc()

--- linux-2.6.21.x/mm/slab.c
+++ linux-2.6.21.x/mm/slab.c
@@ -3202,9 +3202,12 @@ retry:
 
 		if (cpuset_zone_allowed_hardwall(*z, flags) &&
 			cache->nodelists[nid] &&
-			cache->nodelists[nid]->free_objects)
+			cache->nodelists[nid]->free_objects) {
 				obj = ____cache_alloc_node(cache,
 					flags | GFP_THISNODE, nid);
+				if (obj)
+					break;
+		}
 	}
 
 	if (!obj && !(flags & __GFP_NO_GROW)) {
