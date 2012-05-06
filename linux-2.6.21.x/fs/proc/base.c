23b7ceb425cbd2c493cb08e4c5b30ef54d0a6469
sfstudio <sfstudio@mail.ru>
2012-05-06 07:09:30 +0700

    proc: fix lost path declaration in do_proc_readlink after merge code

--- linux-2.6.21.x/fs/proc/base.c
+++ linux-2.6.21.x/fs/proc/base.c
@@ -985,11 +985,12 @@ static int do_proc_readlink(struct dentry *dentry, struct vfsmount *mnt,
 			    char __user *buffer, int buflen)
 {
 	char *tmp = (char*)__get_free_page(GFP_TEMPORARY);
+	char *path;
 	int len;
 
 	if (!tmp)
 		return -ENOMEM;
-		
+
 	path = d_path(dentry, mnt, tmp, PAGE_SIZE);
 	len = PTR_ERR(path);
 	if (IS_ERR(path))
