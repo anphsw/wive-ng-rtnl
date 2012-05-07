8b51152121907a31eed0f0e0e88f68d5de9457de
sfstudio <sfstudio@mail.ru>
2012-05-07 21:32:36 +0700

    small conntrack hash optimize.

--- linux-2.6.21.x/net/netfilter/nf_conntrack_core.c
+++ linux-2.6.21.x/net/netfilter/nf_conntrack_core.c
@@ -237,13 +237,7 @@ static u_int32_t __hash_conntrack(const struct nf_conntrack_tuple *tuple,
 #ifdef CONFIG_NAT_CONE
 	}
 #endif
-
-#if defined(CONFIG_BCM_NAT) || defined(CONFIG_BCM_NAT_MODULE)
-	/* SpeedMod: Change modulo to AND */
-	return jhash_2words(a, b, rnd) & (size-1);
-#else
-	return jhash_2words(a, b, rnd) % size;
-#endif
+	return ((u64)jhash_2words(a, b, rnd) * size) >> 32;
 }
 
 static inline u_int32_t hash_conntrack(const struct nf_conntrack_tuple *tuple)
