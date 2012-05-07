ced62c8861584ae6e28867d053543293eadfe748
sfstudio <sfstudio@mail.ru>
2012-05-08 00:50:17 +0700

    kobject: add kobject_init_ng function

--- linux-2.6.21.x/include/linux/kobject.h
+++ linux-2.6.21.x/include/linux/kobject.h
@@ -71,6 +71,7 @@ static inline const char * kobject_name(const struct kobject * kobj)
 }
 
 extern void kobject_init(struct kobject *);
+extern void kobject_init_ng(struct kobject *kobj, struct kobj_type *ktype);
 extern void kobject_cleanup(struct kobject *);
 
 extern int __must_check kobject_add(struct kobject *);
