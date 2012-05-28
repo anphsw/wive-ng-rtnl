98483ad39f9d117c8c413bf73c0503ed861c7b86
sfstudio <sfstudio@mail.ru>
2012-05-29 06:57:12 +0700

    wifi.sh cosmetics.

--- etc/scripts/wifi.sh
+++ etc/scripts/wifi.sh
@@ -11,20 +11,20 @@ eval `nvram_buf_get 2860 HiPower AutoConnect OperationMode`
 if [ "$HiPower" = "1" ]; then
 # Disable MIMO PowerSave and increase LNA gain
     if [ "$CONFIG_RALINK_RT3052_MP2" = "y" ]; then
-	iwpriv $1 set HiPower=1
+	iwpriv "$1" set HiPower=1
     fi
-    iwpriv $1 set HtMimoPs=0
+    iwpriv "$1" set HtMimoPs=0
 else
 # Enable MIMO PowerSave and set LNA gain to default
     if [ "$CONFIG_RALINK_RT3052_MP2" = "y" ]; then
-	iwpriv $1 set HiPower=0
+	iwpriv "$1" set HiPower=0
     fi
-    iwpriv $1 set HtMimoPs=1
+    iwpriv "$1" set HtMimoPs=1
 fi
 ########################################STAMODE param##########################
 if [ "$OperationMode" = "2" ]; then
     if [ "$AutoConnect" = "1" ]; then
-	iwpriv $1 set AutoReconnect=1
+	iwpriv "$1" set AutoReconnect=1
     fi
   # in sta mode exit
   exit 0
@@ -34,52 +34,52 @@ eval `nvram_buf_get 2860 AutoChannelSelect Channel AP2040Rescan RadioOff \
 			    GreenAP HT_OpMode DfsEnable`
 #########################################ON/OFF param##########################
 if [ "$RadioOff" = "1" ]; then
-    iwpriv $1 set RadioOn=0
+    iwpriv "$1" set RadioOn=0
     echo ">>>> WIFI DISABLED <<<<"
     exit 0
 else
-    iwpriv $1 set RadioOn=1
+    iwpriv "$1" set RadioOn=1
 fi
 ########################################MULTICAST param########################
 if [ "$CONFIG_RT2860V2_AP_IGMP_SNOOP" != "" ]; then
     eval `nvram_buf_get 2860 McastPhyMode McastMcs M2UEnabled`
     if [ "$CONFIG_RT2860V2_MCAST_RATE_SPECIFIC" != "" ]; then
         if [ "$McastPhyMode" != "" ]; then
-	    iwpriv $1 set McastPhyMode=$McastPhyMode
+	    iwpriv "$1" set McastPhyMode=$McastPhyMode
 	fi
 	if [ "$McastMcs" != "" ]; then
-    	    iwpriv $1 set  McastMcs="$McastMcs"
+    	    iwpriv "$1" set  McastMcs="$McastMcs"
 	fi
     fi
     if [ "$M2UEnabled" != "" ]; then
-	iwpriv $1 set IgmpSnEnable="$M2UEnabled"
+	iwpriv "$1" set IgmpSnEnable="$M2UEnabled"
     fi
 fi
 ########################################DFS Setup #############################
 if [ "$CONFIG_RT2860V2_AP_DFS" != "" ] && [ "$OperationMode" = "1" ]; then
     if [ "$DfsEnable" = "1" ]; then
-	iwpriv $1 set RadarStart=1
+	iwpriv "$1" set RadarStart=1
     else
-	iwpriv $1 set RadarStop=1
+	iwpriv "$1" set RadarStop=1
     fi
 fi
 ########################################Channel select#########################
 if [ "$AutoChannelSelect" = "1" ]; then
     # rescan and select optimal channel
     # first need scan
-    iwpriv $1 set SiteSurvey=1
+    iwpriv "$1" set SiteSurvey=1
     # second select channel
-    iwpriv $1 set AutoChannelSel=1
+    iwpriv "$1" set AutoChannelSel=1
 else
     # set channel manual
-    iwpriv $1 set Channel=$Channel
+    iwpriv "$1" set Channel=$Channel
 fi
 ########################################GREEN mode#############################
 if [ "$CONFIG_RT2860V2_AP_GREENAP" != "" ]; then
     if [ "$HT_OpMode" = "1" ] || [ "$GreenAP" = "1" ]; then
-	iwpriv $1 set GreenAP=1
+	iwpriv "$1" set GreenAP=1
     else
-	iwpriv $1 set GreenAP=0
+	iwpriv "$1" set GreenAP=0
     fi
 fi
 #######################################APCLIMODE param ########################
@@ -92,5 +92,5 @@ if [ "$OperationMode" = "3" ]; then
 fi
 ###########################################ALWAYS END##########################
 if [ "$AP2040Rescan" = "1" ]; then
-    iwpriv $1 set AP2040Rescan=1
+    iwpriv "$1" set AP2040Rescan=1
 fi
