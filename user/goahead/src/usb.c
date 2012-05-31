52f89fb616e169ffe1028d53d496d751840419a3
sfstudio <sfstudio@mail.ru>
2012-05-31 07:49:44 +0700

    Some usb 3G patches. Thanks Volt1

--- user/goahead/src/usb.c
+++ user/goahead/src/usb.c
@@ -14,6 +14,7 @@
 #include 	"utils.h"
 #include	"usb.h"
 #include 	"internet.h"
+#include	"helpers.h"
 
 static void storageAdm(webs_t wp, char_t *path, char_t *query);
 static void StorageAddUser(webs_t wp, char_t *path, char_t *query);
@@ -640,80 +641,53 @@ submitUrl = websGetVar(wp, T("submit-url"), T(""));   // hidden page
 #endif
 
 #ifdef CONFIG_USB_MODESWITCH
+
+const parameter_fetch_t usb_modem_args[] =
+{
+	{ T("modem_type"), "MODEMTYPE", 0, T("0") },
+	{ T("modem_port"), "WMODEMPORT", 0, T("AUTO") },
+	{ T("modem_speed"), "MODEMSPEED", 0, T("AUTO") },
+	{ T("modem_mtu"), "MODEMMTU", 0, T("AUTO") },
+	{ T("modem_user"), "MODEMUSERNAME", 0, T("") },
+	{ T("modem_pass"), "MODEMPASSWORD", 0, T("") },
+	{ T("modem_dialn"), "MODEMDIALNUMBER", 0, T("") },
+	{ T("modem_apn"), "APN", 0, T("") },
+	{ T("at_enabled"), "MODEMATENABLED", 2, T("off") },
+	{ T("mdebug_enabled"), "MODEMDEBUG", 2, T("off") },
+	{ T("modem_at1"), "MODEMAT1", 0, T("") },
+	{ T("modem_at2"), "MODEMAT2", 0, T("") },
+	{ T("modem_at3"), "MODEMAT3", 0, T("") },
+	{ NULL, NULL, 0, NULL } // Terminator
+};
+
 static void usbmodem(webs_t wp, char_t *path, char_t *query)
 {
-	char_t *menable;
-	char_t *mtype;
-	char_t *mport;
-	char_t *mspeed;
-	char_t *mmtu;
-	char_t *mname;
-	char_t *mpassw;
-	char_t *mdial;
-	char_t *mapn;
 	char *submitUrl;
-	
-	// fetch from web input
-	menable = websGetVar(wp, T("modem_enabled"), T(""));
-	mtype = websGetVar(wp, T("modem_type"), T(""));
-	mport = websGetVar(wp, T("modem_port"), T(""));
-	mspeed = websGetVar(wp, T("modem_speed"), T(""));
-	mmtu = websGetVar(wp, T("modem_mtu"), T(""));
-	mname = websGetVar(wp, T("modem_user"), T(""));
-	mpassw = websGetVar(wp, T("modem_pass"), T(""));
-	mdial =  websGetVar(wp, T("modem_dialn"), T(""));
-	mapn = websGetVar(wp, T("modem_apn"), T(""));
-	// set to nvram
 	nvram_init(RT2860_NVRAM);
-	nvram_bufset(RT2860_NVRAM, "MODEMENABLED", menable);
-	nvram_bufset(RT2860_NVRAM, "MODEMTYPE", mtype);
-    nvram_bufset(RT2860_NVRAM, "WMODEMPORT", mport);
-	nvram_bufset(RT2860_NVRAM, "MODEMSPEED", mspeed);
-	nvram_bufset(RT2860_NVRAM, "MODEMMTU", mmtu);
-	nvram_bufset(RT2860_NVRAM, "MODEMUSERNAME", mname);
-	nvram_bufset(RT2860_NVRAM, "MODEMPASSWORD", mpassw);
-	nvram_bufset(RT2860_NVRAM, "MODEMDIALNUMBER", mdial);
-	nvram_bufset(RT2860_NVRAM, "APN", mapn);
-	nvram_commit(RT2860_NVRAM);
-	nvram_close(RT2860_NVRAM);
-	
-	if (0 == strcmp(menable, "1"))
+
+	char_t *modem_enabled = websGetVar(wp, T("modem_enabled"), T("off"));
+	if (CHK_IF_CHECKED(modem_enabled))
 	{
-	doSystem("killall -q W61modemhelper");
-	doSystem("killall -q modemhelper");
-	doSystem("killall -q -SIGKILL W61modemhelper");
-	doSystem("killall -q -SIGKILL modemhelper");
-	doSystem("service modemhelper start&");
+		nvram_bufset(RT2860_NVRAM, "MODEMENABLED", "1");
+		setupParameters(wp, usb_modem_args, 0);
 	}
-	else 
-	{
-	doSystem("killall -q W61modemhelper");
-	doSystem("killall -q modemhelper");
-	doSystem("killall -q -SIGKILL W61modemhelper");
-	doSystem("killall -q -SIGKILL modemhelper");
-	doSystem("service modemhelper stop&");
-	}
-submitUrl = websGetVar(wp, T("submit-url"), T(""));   // hidden page
-	if (! submitUrl[0])
-	{
-	// debug print
-	websHeader(wp);
-	websWrite(wp, T("<h2>USB Modem Settings</h2><br>\n"));
-	websWrite(wp, T("modem enabled: %s<br>\n"), menable);
-	websWrite(wp, T("modem type: %s<br>\n"), mtype);
-    websWrite(wp, T("modem port: %s<br>\n"), mport);
-	websWrite(wp, T("modem speed: %s<br>\n"), mspeed);
-	websWrite(wp, T("modem mtu: %s<br>\n"), mmtu);
-	websWrite(wp, T("user name: %s<br>\n"), mname);
-	websWrite(wp, T("user password: %s<br>\n"), mpassw);
-	websWrite(wp, T("dialing number: %s<br>\n"), mdial);
-	websWrite(wp, T("APN: %s<br>\n"), mapn);
-	
-	websFooter(wp);
-	websDone(wp, 200);
-    }
 	else
+		nvram_bufset(RT2860_NVRAM, "MODEMENABLED", "0");
+
+	nvram_commit(RT2860_NVRAM);
+	nvram_close(RT2860_NVRAM);	
+	//kill helpers first sigterm second sigkill	
+	doSystem("killall -q W61modemhelper");
+	doSystem("killall -q modemhelper");
+	doSystem("killall -q -SIGKILL W61modemhelper");
+	doSystem("killall -q -SIGKILL modemhelper");
+	doSystem("service modemhelper restart &");
+	
+submitUrl = websGetVar(wp, T("submit-url"), T(""));   // hidden page
+	if (submitUrl != NULL)
 		websRedirect(wp, submitUrl);
+	else
+		websDone(wp, 200);
 }	
 #endif
 
