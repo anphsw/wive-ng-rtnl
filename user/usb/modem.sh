211ce347d8ca3681f1aa962533b65bc703657670
sfstudio <sfstudio@mail.ru>
2012-08-21 16:27:54 +0700

    Apply some patches for 3G modems support. Thanks [-Alt-]

--- user/usb/modem.sh
+++ user/usb/modem.sh
@@ -9,26 +9,93 @@
 LOG="logger -t 3G"
 
 symlink_modem() {
-    ln -s /dev/$MDEV /dev/modem
-}
-
-symlink_diag() {
-    ln -s /dev/$MDEV /dev/diag
+    if [ ! -e /dev/modem ]; then
+	ln -s /dev/$MDEV /dev/modem
+	MODEMFINDED=1
+    fi
 }
 
 symlink_pcui() {
-    ln -s /dev/$MDEV /dev/pcui
+    if [ ! -e /dev/pcui ]; then
+	ln -s /dev/$MDEV /dev/pcui
+    fi
 }
 
-symlink_pcsc() {
-    ln -s /dev/$MDEV /dev/pcsc
+get_param() {
+    # get local param always
+    eval `nvram_buf_get 2860 MODEMENABLED WMODEMPORT`
+    PPPDIR="/etc/ppp"
+    OPTFILE="$PPPDIR/peers/dialup"
 }
 
-symlink_pcsc() {
-    ln -s /dev/$MDEV /dev/ndis
+create_conf() {
+echo "
+# Modem configuration file
+# format of record - vid : pid : type : data port : UI port
+# vid - vendor id (four hex digits), pid - product id (four hex digits),
+# type - 'C' for CDMA or 'W' for WCDMA/HSPA/HSDPA/HSUPA/GSM
+# data port - main modem interface number (one digit from 0 to 9)
+# UI port - command interface number (one digit from 0 to 9)
+# samples - a12b:1001:W:0:2, 01fc:003b:C:3:N
+
+# HUAWEI Technology
+12d1:1001:W:0:2
+12d1:1003:W:0:1
+12d1:1004:W:0:2
+12d1:1401:W:3:2
+12d1:1402:W:0:2
+12d1:1436:W:0:4
+12d1:140c:W:0:3
+12d1:14ac:W:0:4
+12d1:1506:W:0:1
+
+# ZTE,Incorporated
+19d2:0001:W:0:2
+19d2:0002:W:2:4
+19d2:0003:C:0:1
+19d2:0004:C:0:1
+19d2:0005:C:0:1
+19d2:0006:C:0:1
+19d2:0007:C:0:1
+19d2:0008:C:0:1
+19d2:0009:C:0:1
+19d2:000a:C:0:1
+19d2:0012:W:2:4
+19d2:0015:W:1:3
+19d2:0016:W:2:1
+19d2:0017:W:2:1
+19d2:0018:W:2:1
+19d2:0019:W:2:1
+19d2:0021:W:3:1
+19d2:0024:W:2:4
+19d2:0025:W:2:4
+19d2:0030:W:1:3
+19d2:0031:W:3:1
+19d2:0033:W:4:1
+19d2:0037:W:3:1
+19d2:0042:W:3:1
+19d2:0043:W:3:2
+19d2:0048:W:4:2
+19d2:0049:W:4:2
+19d2:0052:W:3:1
+19d2:0055:W:2:4
+19d2:0061:W:3:1
+19d2:0063:W:3:1
+19d2:0064:W:2:0
+19d2:0066:W:3:1
+19d2:1218:W:2:4
+19d2:2002:W:3:1
+19d2:2003:W:3:1
+" > /etc/modems.conf
+
 }
 
-if [ "$ACTION" = "add" ]; then
+get_param
+if [ ! -e /etc/modems.conf ]; then
+    create_conf
+fi
+
+if [ "$ACTION" = "add" -a ! -e "/dev/modem" ]; then
     for path in $(find /sys/devices -name "$MDEV" | sort -r 2>/dev/null); do
         DEVPATH=${path#/sys}
     done
@@ -40,131 +107,58 @@ if [ "$ACTION" = "add" ]; then
 	done
 	bInterfaceNumber=$(cat /sys${DEVPATH}/../bInterfaceNumber 2>/dev/null)
 	bInterfaceProtocol=$(cat /sys${DEVPATH}/../bInterfaceProtocol 2>/dev/null)
-	if [ "${idVendor}" = "12d1" ]; then
-	    case "${idProduct}" in
-		"1001")
-		    case "$bInterfaceNumber" in
-			"00")
-			    symlink_modem
-			    ;;
-			"01")
-			    symlink_diag
-			    ;;
-			"02")
-			    symlink_pcui
-			    ;;
-			*)
-			    $LOG "Oooops ${idVendor}:${idProduct} $bInterfaceNumber"
-			    ;;
-		    esac
-		    ;;
-		"1003")
-		    case "$bInterfaceNumber" in
-			"00")
-			    symlink_modem
-			    ;;
-			"01")
-			    symlink_pcui
-			    ;;
-			*)
-			    $LOG "Oooops ${idVendor}:${idProduct} $bInterfaceNumber"
-			    ;;
-		    esac
-		    ;;
-		"1004")
-		    case "$bInterfaceNumber" in
-			"00")
-			    symlink_modem
-			    ;;
-			"01")
-			    symlink_diag
-			    ;;
-			"02")
-			    symlink_pcui
-			    ;;
-			"03")
-			    symlink_pcsc
-			    ;;
-			*)
-			    $LOG "Oooops ${idVendor}:${idProduct} $bInterfaceNumber"
-			    ;;
-		    esac
-		    ;;
-		"1401")
-		    case "$bInterfaceNumber" in
-			"00")
-			    symlink_diag
-			    ;;
-			"02")
-			    symlink_pcui
-			    ;;
-			"03")
-			    symlink_modem
-			    ;;
-			*)
-			    $LOG "Oooops ${idVendor}:${idProduct} $bInterfaceNumber"
-			    ;;
-		    esac
-		    ;;
-		"1402")
-		    case "$bInterfaceNumber" in
-			"00")
-			    symlink_modem
-			    ;;
-			"01")
-			    symlink_ndis
-			    ;;
-			"02")
-			    symlink_pcui
-			    ;;
-			*)
-			    $LOG "Oooops ${idVendor}:${idProduct} $bInterfaceNumber"
-			    ;;
-		    esac
-		    ;;
-		"1403")
-		    case "$bInterfaceNumber" in
-			"00")
-			    symlink_pcui
-			    ;;
-			"01")
-			    symlink_ndis
-			    ;;
-			"02")
-			    symlink_diag
-			    ;;
-			"03")
-			    symlink_pcsc
-			    ;;
-			*)
-			    $LOG "Oooops ${idVendor}:${idProduct} $bInterfaceNumber"
-			    ;;
-		    esac
-		    ;;
-		"1506")
-		    case "$bInterfaceNumber" in
-			"00")
-			    symlink_modem
-			    ;;
-			"01")
-			    symlink_ndis
-			    ;;
-			"02")
-			    symlink_pcui
-			    ;;
-                     *)
-                         $LOG "Oooops ${idVendor}:${idProduct} $bInterfaceNumber"
-                         ;;
-		    esac
-		    ;;
-		*)
-		    $LOG "Unknow Huawei modem ${idVendor}:${idProduct}"
-		    ;;
-	    esac
+	
+	modem_conf=`cat /etc/modems.conf | awk "/^${idVendor}\:${idProduct}/ {split(\\$0,a,\":\"); print \"mVendor=\"a[1] \" mProduct=\"a[2] \" mType=\"a[3] \" mModemIf=\'0\"a[4] \"\' mUserIf=\'0\"a[5] \"\'\"}"`
+	eval $modem_conf
+	
+	if [ $bInterfaceNumber = $mModemIf ]; then 
+	    symlink_modem
 	fi
-    fi
-else
 
+	if [ $bInterfaceNumber = $mUserIf ]; then 
+	    symlink_pcui
+	fi
+	
+    fi
+
+    if [ "$MDEV" = "$WMODEMPORT" ] || [ "$WMODEMPORT" = "AUTO" -a "$MODEMFINDED" = "1" ]; then
+	$LOG "Modem added. Starting helper service -$MDEV-"
+	service modemhelper start
+    fi
+else	
+	if [ -e "/dev/modem" ]; then
+	    DEVMODEMTTY=`ls -l  /dev/modem | cut -f 2 -d ">" | sed "s/ \/dev\///"`
+	fi
+
+	if [ -e "/dev/pcui" ]; then
+	    DEVPCUITTY=`ls -l  /dev/pcui | cut -f 2 -d ">" | sed "s/ \/dev\///"`
+	fi
+
+	
+	PPPDTTY=`cat $OPTFILE | grep $MDEV | sed 's/^[ \t]*//;s/[ \t]*$//' `
+
+	if [ "$DEVPCUITTY" = "$MDEV" ]; then
+	    $LOG "Modem removed. Deleting symlink."
+	    rm -f /dev/pcui
+	fi
+
+	if [ "$DEVMODEMTTY" = "$MDEV" ]; then
+	    $LOG "Modem removed. Deleting symlink."
+	    rm -f /dev/modem
+	    if [ -n `cat $OPTFILE | grep /dev/modem` ]; then
+		NEEDSTOPHELPER=1
+	    fi
+	fi
+
+	if [ "$PPPDTTY" = "/dev/$MDEV" ]; then
+		$LOG "Modem removed."
+		NEEDSTOPHELPER=1
+	fi
+
+	if [ "$NEEDSTOPHELPER" = "1" ]; then
+	    $LOG "Stopping helper service."
+	    service modemhelper modemremove
+	fi
 fi
 
 exit 0
