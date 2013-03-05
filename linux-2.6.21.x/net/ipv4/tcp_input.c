b7fe654e4f470695eaa773e0d0697d391d727521
sfstudio <sfstudio@mail.ru>
2013-03-06 04:54:55 +0700

    kernel: tcp: fix double-counted receiver RTT when leaving receiver fast path.

--- linux-2.6.21.x/net/ipv4/tcp_input.c
+++ linux-2.6.21.x/net/ipv4/tcp_input.c
@@ -4241,6 +4241,9 @@ int tcp_rcv_established(struct sock *sk, struct sk_buff *skb,
 				if (tcp_checksum_complete_user(sk, skb))
 					goto csum_error;
 
+				if ((int)skb->truesize > sk->sk_forward_alloc)
+					goto step5;
+
 				/* Predicted packet is in window by definition.
 				 * seq == rcv_nxt and rcv_wup <= rcv_nxt.
 				 * Hence, check seq<=rcv_wup reduces to:
@@ -4251,9 +4254,6 @@ int tcp_rcv_established(struct sock *sk, struct sk_buff *skb,
 					tcp_store_ts_recent(tp);
 
 				tcp_rcv_rtt_measure_ts(sk, skb);
-
-				if ((int)skb->truesize > sk->sk_forward_alloc)
-					goto step5;
 
 				NET_INC_STATS_BH(LINUX_MIB_TCPHPHITS);
 
