ee759da5dae471d54ed23ee0ee18bcaf59178c54
sfstudio <sfstudio@mail.ru>
2013-04-08 15:52:26 +0700

    oops small fix last patch.

--- linux-2.6.21.x/net/ipv4/ip_fragment.c
+++ linux-2.6.21.x/net/ipv4/ip_fragment.c
@@ -761,7 +761,7 @@ static int ip_frag_reasm(struct ipq *qp, struct sk_buff *prev,
 
 	iph = head->nh.iph;
 	/* max_size != 0 implies at least one fragment had IP_DF set */
-	iph->frag_off = qp->q.max_size ? htons(IP_DF) : 0;
+	iph->frag_off = qp->max_size ? htons(IP_DF) : 0;
 	iph->tot_len = htons(len);
 	IP_INC_STATS_BH(IPSTATS_MIB_REASMOKS);
 	qp->fragments = NULL;
