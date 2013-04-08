60beaff64a1856d93db90983b54fa63814c4c2b2
sfstudio <sfstudio@mail.ru>
2013-04-09 06:17:58 +0700

    gather dma and hw_vlan offload not work correct in RT3052 if PPE enabled.

--- linux-2.6.21.x/drivers/net/raeth/raether.c
+++ linux-2.6.21.x/drivers/net/raeth/raether.c
@@ -413,7 +413,7 @@ void forward_config(struct net_device *dev)
 
 #ifdef CONFIG_RAETH_HW_VLAN_TX
 #ifdef CONFIG_VLAN_8021Q_DOUBLE_TAG
-	if (!vlan_double_tag)
+	if ((!vlan_double_tag) && (ra_sw_nat_hook_rx == NULL))
 #endif
 	{
     	    printk("raeth: vlan hardware offload enabled\n");
@@ -470,7 +470,7 @@ void forward_config(struct net_device *dev)
 
 #ifdef CONFIG_RAETH_HW_VLAN_TX
 #ifdef CONFIG_VLAN_8021Q_DOUBLE_TAG
-	if (!vlan_double_tag)
+	if ((!vlan_double_tag) && (ra_sw_nat_hook_rx == NULL))
 #endif
 	    dev->features |= NETIF_F_HW_VLAN_TX;
 #endif
@@ -847,7 +847,7 @@ static inline int rt2880_eth_send(struct net_device* dev, struct sk_buff *skb, i
 
 #ifdef CONFIG_RAETH_HW_VLAN_TX
 #ifdef CONFIG_VLAN_8021Q_DOUBLE_TAG
-	if (!vlan_double_tag)
+	if ((!vlan_double_tag) && (ra_sw_nat_hook_rx == NULL))
 #endif
 	{
 	    if(vlan_tx_tag_present(skb)) {
@@ -906,7 +906,7 @@ static inline int rt2880_eth_send(struct net_device* dev, struct sk_buff *skb, i
 
 #ifdef CONFIG_RAETH_HW_VLAN_TX
 #ifdef CONFIG_VLAN_8021Q_DOUBLE_TAG
-	if (!vlan_double_tag)
+	if ((!vlan_double_tag) && (ra_sw_nat_hook_rx == NULL))
 #endif
 	{
 	    if(vlan_tx_tag_present(skb)) {
