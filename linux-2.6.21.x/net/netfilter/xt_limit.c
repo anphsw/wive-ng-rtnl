759e22c43c18d338bbabf038422aecaf76204396
sfstudio <sfstudio@mail.ru>
2013-04-21 06:18:48 +0700

    kernel: netfilter: xt_limit: have r->cost != 0 case work

--- linux-2.6.21.x/net/netfilter/xt_limit.c
+++ linux-2.6.21.x/net/netfilter/xt_limit.c
@@ -127,11 +127,11 @@ ipt_limit_checkentry(const char *tablename,
 
 	/* For SMP, we only want to use one set of state. */
 	r->master = priv;
+	/* User avg in seconds * XT_LIMIT_SCALE: convert to jiffies *
+	   128. */
+	priv->prev = jiffies;
+	priv->credit = user2credits(r->avg * r->burst); /* Credits full. */
 	if (r->cost == 0) {
-		/* User avg in seconds * XT_LIMIT_SCALE: convert to jiffies *
-		   128. */
-		priv->prev = jiffies;
-		priv->credit = user2credits(r->avg * r->burst); /* Credits full. */
 		r->credit_cap = priv->credit; /* Credits full. */
 		r->cost = user2credits(r->avg);
 	}
