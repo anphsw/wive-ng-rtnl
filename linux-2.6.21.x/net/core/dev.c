568a2ddd5286b71f6cf4bbdfa6f135117cf83146
sfstudio <sfstudio@mail.ru>
2013-05-12 08:18:40 +0700

    Rebuild 4.1.5 - kernel: net/core: fix refcount leak hack.

--- linux-2.6.21.x/net/core/dev.c
+++ linux-2.6.21.x/net/core/dev.c
@@ -3252,23 +3252,25 @@ static void netdev_wait_allrefs(struct net_device *dev)
 		 *This is not clean hack. May be resource leak cardmap ppp module not touch refcnt and not need check this
 		*/
 		if (time_after(jiffies, warning_time + 10 * HZ)) {
+
 			warning_time = jiffies;
-			/* wait count exeed - break and warn of leak */
-			if (count > 2) {
-			    refcnt = 0;
-			    printk(KERN_EMERG "unregister_netdevice %s refcnt leak. need fix.", dev->name);
-			    break;
-			}
 
 			/* refcnt freed ok ? - break without warn or reread refcnt and repeat wait */
-                        if (!refcnt)
+                        if (!refcnt) {
 			    break;
-			else
+			} else {
 			    refcnt = atomic_read(&dev->refcnt);
+			    count++;
+			}
 
-			count++;
-#endif
+			/* wait count exeed - break and warn of leak */
+			if (count > 3) {
+			    refcnt = 0;
+			    printk(KERN_EMERG "unregister_netdevice %s refcnt leak. need fix. Usage count = %d\n", dev->name, refcnt);
+			    break;
+			}
 		}
+#endif
 	}
 }
 
