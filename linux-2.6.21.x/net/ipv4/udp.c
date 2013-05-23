0d54b34ca613e879b4a570339c4980c7aa257d3f
sfstudio <sfstudio@mail.ru>
2013-05-23 07:16:29 +0700

    kernel: udp: fix udp short packet false positive.

--- linux-2.6.21.x/net/ipv4/udp.c
+++ linux-2.6.21.x/net/ipv4/udp.c
@@ -1156,7 +1156,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct hlist_head udptable[],
 		   int proto)
 {
 	struct sock *sk;
-	struct udphdr *uh = udp_hdr(skb);
+	struct udphdr *uh;
 	unsigned short ulen;
 	struct rtable *rt = (struct rtable*)skb->dst;
 	__be32 saddr, daddr;
@@ -1167,6 +1167,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct hlist_head udptable[],
 	if (!pskb_may_pull(skb, sizeof(struct udphdr)))
 		goto drop;		/* No space for header. */
 
+	uh   = udp_hdr(skb);
 	ulen = ntohs(uh->len);
 	saddr = ip_hdr(skb)->saddr;
 	daddr = ip_hdr(skb)->daddr;
