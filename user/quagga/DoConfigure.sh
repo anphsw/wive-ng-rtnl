e3cb13b9483adb408bc487066213e44683bf5afe
sfstudio <sfstudio@mail.ru>
2013-09-12 01:22:18 +0700

    quagga remove filesystem dir after call romfs target.

--- user/quagga/DoConfigure.sh
+++ user/quagga/DoConfigure.sh
@@ -13,8 +13,8 @@ fi
 
 CONFOPTS="--host=mipsel-linux"
 CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem"
-CONFOPTS="$CONFOPTS --disable-bgpd --disable-babeld --disable-ripngd "
-CONFOPTS="$CONFOPTS --disable-ospfapi --disable-ospfclient--disable-ospf6d --disable-ospfd"
+CONFOPTS="$CONFOPTS --disable-bgpd --disable-babeld --disable-ripngd"
+CONFOPTS="$CONFOPTS --disable-ospfapi --disable-ospfclient --disable-ospfd --disable-ospf6d"
 CONFOPTS="$CONFOPTS --disable-dependency-tracking"
 
 ./configure $CONFOPTS
