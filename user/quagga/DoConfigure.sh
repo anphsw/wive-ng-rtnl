d3f93c7118ea585e4adb3600d41cc477d9e227b0
sfstudio <sfstudio@mail.ru>
2013-09-12 00:54:10 +0700

    quagga do not disable ipv6 support.

--- user/quagga/DoConfigure.sh
+++ user/quagga/DoConfigure.sh
@@ -12,7 +12,7 @@ if [ ! -f $APROOTDIR/Makefile.in ]; then
 fi
 
 CONFOPTS="--host=mipsel-linux"
-CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem --disable-ipv6"
+CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem"
 CONFOPTS="$CONFOPTS --disable-bgpd --disable-babeld --disable-ripngd "
 CONFOPTS="$CONFOPTS --disable-ospfapi --disable-ospfclient--disable-ospf6d --disable-ospfd"
 CONFOPTS="$CONFOPTS --disable-dependency-tracking"
