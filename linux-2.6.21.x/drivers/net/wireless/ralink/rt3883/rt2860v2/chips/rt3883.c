81043fc5a9ce354e640185a4e05961fc214eb43a
sfstudio <sfstudio@mail.ru>
2013-09-13 10:18:12 +0700

    kernel: rt3883: backport default reg tables init.

--- linux-2.6.21.x/drivers/net/wireless/ralink/rt3883/rt2860v2/chips/rt3883.c
+++ linux-2.6.21.x/drivers/net/wireless/ralink/rt3883/rt2860v2/chips/rt3883.c
@@ -80,14 +80,14 @@ REG_PAIR   RT3883_RFRegTable[] = {
 	{RF_R07, 0x00},
 	{RF_R08, 0x5B},
 	{RF_R09, 0x08},
-	{RF_R10, 0xD3}, // Gary, 2010-02-12
+	{RF_R10, 0xD3}, /* Gary, 2010-02-12 */
 	{RF_R11, 0x48},
 	{RF_R12, 0x1A},	/* Gary, 2011-03-10 */
 	{RF_R13, 0x12},
 	{RF_R14, 0x00},
 	{RF_R15, 0x00},
 	{RF_R16, 0x00},
-/*	{RF_R17, 0x26}, By EEPROM Frequency offset */
+	{RF_R17, 0x26},
 	{RF_R18, 0x40},
 	{RF_R19, 0x00},
 	{RF_R20, 0x00},
@@ -108,13 +108,13 @@ REG_PAIR   RT3883_RFRegTable[] = {
 	{RF_R35, 0x00},
 	{RF_R36, 0x00},
 	{RF_R37, 0x00},
-	{RF_R38, 0x86},	// Gary, 2010-02-12
+	{RF_R38, 0x86},	/* Gary, 2010-02-12 */
 	{RF_R39, 0x23},
 	{RF_R40, 0x00},
 	{RF_R41, 0x00},
 	{RF_R42, 0x00},
 	{RF_R43, 0x00},
-	{RF_R44, 0x93},	// Gary, 2009-12-08
+	{RF_R44, 0x93},	/* Gary, 2009-12-08 */
 	{RF_R45, 0xBB},
 	{RF_R46, 0x60},
 	{RF_R47, 0x00},
@@ -139,23 +139,33 @@ REG_PAIR   RT3883_RFRegTable[] = {
 UCHAR RT3883_NUM_RF_REG_PARMS = (sizeof(RT3883_RFRegTable) / sizeof(REG_PAIR));
 
 REG_PAIR   RT3883_BBPRegTable[] = {
-	{BBP_R4,		0x50},
-	{BBP_R47,		0x07},	// 5b ADC is used to measure chip temp through PTAT voltage
-	{BBP_R92,		0x02},	//improve 10~20Mbps peak throughput, requested by Henry 2009-11-26
-	{BBP_R103,		0xC0}, 
-	{BBP_R104,		0x92},	//improve 10~20Mbps peak throughput, requested by Henry 2009-11-26
-	{BBP_R105,		0x04},	// RT2883 default is 0x4
-	{BBP_R137,		0x0F},  // julian suggest make the RF output more stable
-	{BBP_R163,		0xBC},	// Enable saving of Explicit and Implicit profiles
+	{BBP_R4,		0x50}, /* 3883 need to */
+	{BBP_R47,		0x48},  /* ALC Functions change from 0x7 to 0x48 Baron suggest */
 
-	{BBP_R179,		0x02},	// Set ITxBF timeout to 0x9C40=1000msec
+	{BBP_R86,		0x46},	/* for peak throughput, Henry 2009-12-23 */
+	{BBP_R88,		0x90},	/* for rt3883 middle range, Henry 2009-12-31 */
+
+	{BBP_R92,		0x02},  /* middle range issue, Rory @2008-01-28 */
+
+	{BBP_R103,  		0xC0},
+	{BBP_R104,		0x92},
+	{BBP_R105,		0x34},
+#ifdef DOT11_N_SUPPORT
+	{BBP_R106,		0x12},	// 40M=2, 20M=2. Fix 20M SGI STBC problem
+#endif /* DOT11_N_SUPPORT */
+	{BBP_R120,		0x50},	/* for long range -2db, Gary 2010-01-22 */
+	{BBP_R137,		0x0F},  /* Julian suggest make the RF output more stable */
+	{BBP_R163,		0x9D}, /* Enable TxBf modes by default, Gary, 2010-06-02 */
+
+	{BBP_R179,		0x02},	/* Set ITxBF timeout to 0x9C40=1000msec */
 	{BBP_R180,		0x00},
 	{BBP_R182,		0x40},
 	{BBP_R180,		0x01},
 	{BBP_R182,		0x9C},
+
 	{BBP_R179,		0x00},
 
-	{BBP_R142,		0x04},	// Reprogram the inband interface to put right values in RXWI
+	{BBP_R142,		0x04},	/* Reprogram the inband interface to put right values in RXWI */
 	{BBP_R143,		0x3b},
 	{BBP_R142,		0x06},
 	{BBP_R143,		0xA0},
@@ -163,8 +173,7 @@ REG_PAIR   RT3883_BBPRegTable[] = {
 	{BBP_R143,		0xA1},
 	{BBP_R142,		0x08},
 	{BBP_R143,		0xA2},
-
-	{BBP_R106,		0x12},	// 40M=2, 20M=2. Fix 20M SGI STBC problem
+	{BBP_R148,		0xC8},	/* Gary, 2010-2-12 */
 };
 
 UCHAR RT3883_NUM_BBP_REG_PARMS = (sizeof(RT3883_BBPRegTable) / sizeof(REG_PAIR));
