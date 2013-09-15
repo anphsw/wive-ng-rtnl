ed7c7f3182a949aa7bc1d870d77a1bef9ffdbbbd
sfstudio <sfstudio@mail.ru>
2013-09-16 03:24:34 +0700

    fix nr_cpu_ids use in dev.c.

--- linux-2.6.21.x/net/core/dev.c
+++ linux-2.6.21.x/net/core/dev.c
@@ -2286,7 +2286,7 @@ static struct netif_rx_stats *softnet_get_online(loff_t *pos)
 {
 	struct netif_rx_stats *rc = NULL;
 
-	while (*pos < NR_CPUS)
+	while (*pos < nr_cpu_ids)
 		if (cpu_online(*pos)) {
 			rc = &per_cpu(netdev_rx_stat, *pos);
 			break;
