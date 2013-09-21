73cf360c974f0fa2b56730e8f0c496447efa924a
sfstudio <sfstudio@mail.ru>
2013-09-22 05:04:59 +0700

    kernel:  remove odd and misleading comments from uio.h

--- linux-2.6.21.x/include/linux/uio.h
+++ linux-2.6.21.x/include/linux/uio.h
@@ -13,10 +13,6 @@
  *		2 of the License, or (at your option) any later version.
  */
 
-
-/* A word of warning: Our uio structure will clash with the C library one (which is now obsolete). Remove the C
-   library one from sys/uio.h if you have a very old library set */
-
 struct iovec
 {
 	void __user *iov_base;	/* BSD uses caddr_t (1003.1g requires void *) */
@@ -29,11 +25,6 @@ struct iovec
  
 #define UIO_FASTIOV	8
 #define UIO_MAXIOV	1024
-#if 0
-#define UIO_MAXIOV	16	/* Maximum iovec's in one operation 
-				   16 matches BSD */
-                                /* Beg pardon: BSD has 1024 --ANK */
-#endif
 
 #ifdef __KERNEL__
 
