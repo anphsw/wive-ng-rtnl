3c180147412fef73c922ebcdf992076b2a04bc95
sfstudio <sfstudio@mail.ru>
2013-09-22 07:57:22 +0700

    miniupnpd: fix getiffadrs not found and enable ipv6 support per default.

--- user/miniupnp/genconfig.sh
+++ user/miniupnp/genconfig.sh
@@ -32,7 +32,7 @@ done
 LEASEFILE=1
 
 # need move to new glibc version
-#IPV6=`cat ../../linux/.config | grep "CONFIG_IPV6=y" -c`
+IPV6=`cat ../../linux/.config | grep "CONFIG_IPV6=y" -c`
 
 RM="rm -f"
 MV="mv"
