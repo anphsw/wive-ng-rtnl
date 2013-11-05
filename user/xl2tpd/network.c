ed4cdca083c92d8b1b33729364b380e8935318ca
sfstudio <sfstudio@mail.ru>
2013-11-06 00:13:16 +0700

    Cleanup.

--- user/xl2tpd/network.c
+++ user/xl2tpd/network.c
@@ -104,7 +104,6 @@ int init_network (void)
 
     arg=1;
     setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &arg, sizeof(arg));
-    setsockopt(server_socket, SOL_SOCKET, SO_NO_CHECK, &arg, sizeof(arg));
 
     if (bind (server_socket, (struct sockaddr *) &server, sizeof (server)))
     {
