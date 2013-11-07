de0d10c57b1eff976ea4b3a67fd8c98643764771
sfstudio <sfstudio@mail.ru>
2013-11-07 17:05:04 +0700

    kernel: loopback: packet drops accounting

--- linux-2.6.21.x/drivers/net/loopback.c
+++ linux-2.6.21.x/drivers/net/loopback.c
@@ -59,6 +59,7 @@
 struct pcpu_lstats {
 	unsigned long packets;
 	unsigned long bytes;
+	unsigned long drops;
 };
 static DEFINE_PER_CPU(struct pcpu_lstats, pcpu_lstats);
 
@@ -71,19 +72,23 @@ static DEFINE_PER_CPU(struct pcpu_lstats, pcpu_lstats);
 static int loopback_xmit(struct sk_buff *skb, struct net_device *dev)
 {
 	struct pcpu_lstats *lb_stats;
+	int len;
 
 	skb_orphan(skb);
 
-	skb->protocol = eth_type_trans(skb,dev);
+	skb->protocol = eth_type_trans(skb, dev);
 #ifndef LOOPBACK_MUST_CHECKSUM
 	skb->ip_summed = CHECKSUM_UNNECESSARY;
 #endif
 	/* it's OK to use __get_cpu_var() because BHs are off */
 	lb_stats = &__get_cpu_var(pcpu_lstats);
-	lb_stats->bytes += skb->len;
-	lb_stats->packets++;
 
-	netif_rx(skb);
+	len = skb->len;
+	if (likely(netif_rx(skb) == NET_RX_SUCCESS)) {
+		lb_stats->bytes += len;
+		lb_stats->packets++;
+	} else
+		lb_stats->drops++;
 
 	return 0;
 }
@@ -95,6 +100,7 @@ static struct net_device_stats *get_stats(struct net_device *dev)
 	struct net_device_stats *stats = &loopback_stats;
 	unsigned long bytes = 0;
 	unsigned long packets = 0;
+	unsigned long drops = 0;
 	int i;
 
 	for_each_possible_cpu(i) {
@@ -103,11 +109,14 @@ static struct net_device_stats *get_stats(struct net_device *dev)
 		lb_stats = &per_cpu(pcpu_lstats, i);
 		bytes   += lb_stats->bytes;
 		packets += lb_stats->packets;
+		drops   += lb_stats->drops;
 	}
 	stats->rx_packets = packets;
 	stats->tx_packets = packets;
-	stats->rx_bytes = bytes;
-	stats->tx_bytes = bytes;
+	stats->rx_dropped = drops;
+	stats->rx_errors  = drops;
+	stats->rx_bytes   = bytes;
+	stats->tx_bytes   = bytes;
 	return stats;
 }
 
