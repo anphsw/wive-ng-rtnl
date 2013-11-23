729fd3d2231e58ad7999a6efe0fffa5e34907924
sfstudio <sfstudio@mail.ru>
2013-11-23 22:03:21 +0700

    dropbear: fix cleanup and allow large file support.

--- user/dropbear/DoConfigure.sh
+++ user/dropbear/DoConfigure.sh
@@ -16,6 +16,6 @@ CONFOPTS="$CONFOPTS --disable-shadow --disable-pam"
 CONFOPTS="$CONFOPTS --disable-lastlog --disable-utmp --disable-utmpx"
 CONFOPTS="$CONFOPTS --disable-wtmp --disable-wtmpx --disable-loginfunc"
 CONFOPTS="$CONFOPTS --disable-pututline --disable-pututxline"
-CONFOPTS="$CONFOPTS --disable-largefile --prefix=$APROOTDIR/filesystem"
+CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem"
 
 ./configure $CONFOPTS
