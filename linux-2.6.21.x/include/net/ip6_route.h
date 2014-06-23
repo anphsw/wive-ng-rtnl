92f49abcb5eff31213d81ab626af5b975e873f22
sfstudio <sfstudio@mail.ru>
2014-06-23 18:49:18 +0700

    add lost include.

--- linux-2.6.21.x/include/net/ip6_route.h
+++ linux-2.6.21.x/include/net/ip6_route.h
@@ -169,6 +169,7 @@ static inline int ipv6_unicast_destination(struct sk_buff *skb)
 	return rt->rt6i_flags & RTF_LOCAL;
 }
 
+#include <net/ipv6.h>
 static inline int rt6_need_strict(const struct in6_addr *daddr)
 {
 	return ipv6_addr_type(daddr) &
