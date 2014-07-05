b16dbe41805f6ea623bb2c97070bb4615e1e07ea
sfstudio <sfstudio@mail.ru>
2014-07-05 19:46:39 +0700

    kernel: pppol2tp: pre 2.6.22 kernels compatibility, not need in wive

--- linux-2.6.21.x/drivers/net/pppol2tp.c
+++ linux-2.6.21.x/drivers/net/pppol2tp.c
@@ -115,13 +115,15 @@
 /* Old L2TP daemons semi-compatibility */
 //#define PPPOL2TP_UDP_CONNECT
 
-/* Pre 2.6.22 kernels compatibility */
+#if 0
+/* Pre 2.6.22 kernels compatibility, not need in wive */
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,22)
 #define udp_hdr(skb) (struct udphdr *) (skb)->h.raw
 #define skb_network_header(skb) skb->nh.raw
 #define skb_transport_header(skb) skb->h.raw
 #define skb_reset_network_header(skb) (skb)->nh.raw = (skb)->data
 #define skb_reset_transport_header(skb) (skb)->h.raw = (skb)->data
+#endif
 #endif
 
 /* L2TP header constants */
