f50eb15750f9f9f7adbff37f2f54fb7ac78228db
sfstudio <sfstudio@mail.ru>
2014-07-19 06:22:56 +0700

    pppol2tp: oops, need return anstead nop, gcc-4.8 disable optimizatinon instead... brrrr

--- linux-2.6.21.x/drivers/net/pppol2tp.c
+++ linux-2.6.21.x/drivers/net/pppol2tp.c
@@ -1312,7 +1312,7 @@ static void pppol2tp_tunnel_destruct(struct sock *sk)
 	pppol2tp_tunnel_closeall(tunnel);
 
 end:
-    ;
+    return;
 }
 
 /* Really kill the socket. (Called from sock_put if refcnt == 0.)
@@ -1359,7 +1359,7 @@ static void pppol2tp_session_destruct(struct sock *sk)
 	if (session != NULL)
 		kfree(session);
 out:
-    ;
+    return;
 }
 
 /* Called when the PPPoX socket (session) is closed.
