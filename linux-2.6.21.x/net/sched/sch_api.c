fe9e5ca3f91c05bae2a712c7699bb51c54b2ee60
sfstudio <sfstudio@mail.ru>
2014-08-07 11:28:20 +0700

    kernel: kernel: pkt_sched: add cond_resched() to qdisc_alloc_handle

--- linux-2.6.21.x/net/sched/sch_api.c
+++ linux-2.6.21.x/net/sched/sch_api.c
@@ -293,6 +293,7 @@ static u32 qdisc_alloc_handle(struct net_device *dev)
 		autohandle += TC_H_MAKE(0x10000U, 0);
 		if (autohandle == TC_H_MAKE(TC_H_ROOT, 0))
 			autohandle = TC_H_MAKE(0x80000000U, 0);
+		cond_resched();
 	} while	(qdisc_lookup(dev, autohandle) && --i > 0);
 
 	return i>0 ? autohandle : 0;
