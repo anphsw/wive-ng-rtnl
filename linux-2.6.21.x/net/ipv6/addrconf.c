76f098275fdbbfdcf4d53e3b0ea629ab01740577
sfstudio <sfstudio@mail.ru>
2014-08-15 01:09:54 +0700

    kernel: ipv6: remove optimistic DAD flag test in ipv6_add_addr().

--- linux-2.6.21.x/net/ipv6/addrconf.c
+++ linux-2.6.21.x/net/ipv6/addrconf.c
@@ -556,17 +556,6 @@ ipv6_add_addr(struct inet6_dev *idev, const struct in6_addr *addr, int pfxlen,
 	ifa->cstamp = ifa->tstamp = jiffies;
 
 	ifa->rt = rt;
-
-	/*
-	 * part one of RFC 4429, section 3.3
-	 * We should not configure an address as
-	 * optimistic if we do not yet know the link
-	 * layer address of our nexhop router
-	 */
-
-	if (rt->rt6i_nexthop == NULL)
-		ifa->flags &= ~IFA_F_OPTIMISTIC;
-
 	ifa->idev = idev;
 	in6_dev_hold(idev);
 	/* For caller */
