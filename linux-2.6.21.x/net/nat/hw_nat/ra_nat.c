198a90a13aa158494f3a5f8f8385b738dca70387
sfstudio <sfstudio@mail.ru>
2014-08-17 15:48:31 +0700

    lost (

--- linux-2.6.21.x/net/nat/hw_nat/ra_nat.c
+++ linux-2.6.21.x/net/nat/hw_nat/ra_nat.c
@@ -1443,7 +1443,7 @@ static int32_t PpeFillInL4Info(struct sk_buff * skb, struct FoeEntry * foe_entry
 			foe_entry->ipv4_hnapt.bfib1.udp = TCP;
 		} else if (PpeParseResult.iph.protocol == IPPROTO_UDP) {
 			    if (ppe_udp_bug) {
-			        if (!PpeParseResult.uh.check) ||
+			        if ((!PpeParseResult.uh.check) ||
 				    (PpeParseResult.uh.dest == __constant_htons(500) ||	// IPSec IKE
 				    PpeParseResult.uh.dest == __constant_htons(4500) ||	// IPSec NAT-T
 				    PpeParseResult.uh.dest == __constant_htons(1701)) {	// L2TP
