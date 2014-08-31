8d4b37854b3ca11f8949423c78af3a2c2d891151
sfstudio <sfstudio@mail.ru>
2014-08-31 19:16:01 +0700

    kernel: skbuff: small cleanup for help gcc optimize.

--- linux-2.6.21.x/net/core/skbuff.c
+++ linux-2.6.21.x/net/core/skbuff.c
@@ -882,23 +882,23 @@ int skb_pad(struct sk_buff *skb, int pad)
 	ntail = skb->data_len + pad - (skb->end - skb->tail);
 	if (likely(skb_cloned(skb) || ntail > 0)) {
 		err = pskb_expand_head(skb, 0, ntail, GFP_ATOMIC);
-		if (unlikely(err))
-			goto free_skb;
+		if (unlikely(err)) {
+			kfree_skb(skb);
+			return err;
+		}
 	}
 
 	/* FIXME: The use of this function with non-linear skb's really needs
 	 * to be audited.
 	 */
 	err = skb_linearize(skb);
-	if (unlikely(err))
-		goto free_skb;
+	if (unlikely(err)) {
+		kfree_skb(skb);
+		return err;
+	}
 
 	memset(skb->data + skb->len, 0, pad);
 	return 0;
-
-free_skb:
-	kfree_skb(skb);
-	return err;
 }
 
 /* Trims skb to length len. It can change skb pointers.
