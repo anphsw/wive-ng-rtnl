ae7edb213548ace5f6901d216965f327205a1b31
sfstudio <sfstudio@mail.ru>
2014-10-07 15:16:03 +0700

    small optimize.

--- linux-2.6.21.x/net/core/skbuff.c
+++ linux-2.6.21.x/net/core/skbuff.c
@@ -194,7 +194,9 @@ struct sk_buff FASTPATHNET *__alloc_skb(unsigned int size, gfp_t gfp_mask,
 	}
 
 #if defined(CONFIG_RA_HW_NAT) || defined(CONFIG_RA_HW_NAT_MODULE)
+#if defined(HNAT_USE_HEADROOM)
 	DO_FAST_CLEAR_FOE(skb); // fast clear FoE info header
+#endif
 #endif
 	return skb;
 }
@@ -300,6 +302,11 @@ static void kfree_skbmem(struct sk_buff *skb)
 		if (atomic_dec_and_test(fclone_ref))
 			kmem_cache_free(skbuff_fclone_cache, other);
 		break;
+#if defined(CONFIG_RA_HW_NAT) || defined(CONFIG_RA_HW_NAT_MODULE)
+	default:
+		if(IS_MAGIC_TAG_VALID(skb) || FOE_MAGIC_TAG(skb) == FOE_MAGIC_PPE)
+		    memset(FOE_INFO_START_ADDR(skb), 0, FOE_INFO_LEN);
+#endif
 	};
 }
 
@@ -342,10 +349,6 @@ static void skb_release_all(struct sk_buff *skb)
 
 void FASTPATHNET __kfree_skb(struct sk_buff *skb)
 {
-#if defined(CONFIG_RA_HW_NAT) || defined(CONFIG_RA_HW_NAT_MODULE)
-	if(IS_MAGIC_TAG_VALID(skb) || (FOE_MAGIC_TAG(skb) == FOE_MAGIC_PPE))
-		memset(FOE_INFO_START_ADDR(skb), 0, FOE_INFO_LEN);
-#endif
 	skb_release_all(skb);
 	kfree_skbmem(skb);
 }
